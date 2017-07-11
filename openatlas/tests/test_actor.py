addeab03b943feed4b2e28a1f2dd8f33dd1eb24c
Alexander Watzinger <alexander.watzinger@craws.net>
2017-07-11 22:52:21 +0200

    unit tests for source, actor and place

--- openatlas/tests/test_actor.py
+++ openatlas/tests/test_actor.py
@@ -5,5 +5,20 @@ from openatlas.test_base import TestBaseCase
 class ActorTests(TestBaseCase):
 
     def test_actor(self):
+        rv = self.app.get('/actor/insert/E21')
+        assert '+ Person' in rv.data
+        form_data = {'name': 'Test actor', 'description': 'Actor description'}
+        rv = self.app.post('/actor/insert/E21', data=form_data)
+        actor_id = rv.location.split('/')[-1]
+        form_data['continue_'] = 'yes'
+        rv = self.app.post('/actor/insert/E21', data=form_data, follow_redirects=True)
+        assert 'Entity created' in rv.data
         rv = self.app.get('/actor')
-        #assert 'Overview' in rv.data
+        assert 'Test actor' in rv.data
+        rv = self.app.get('/actor/update/' + actor_id)
+        assert 'Test actor' in rv.data
+        form_data['name'] = 'Test actor updated'
+        rv = self.app.post('/actor/update/' + actor_id, data=form_data, follow_redirects=True)
+        assert 'Test actor updated' in rv.data
+        rv = self.app.get('/actor/delete/' + actor_id, follow_redirects=True)
+        assert 'Entity deleted' in rv.data
--- openatlas/tests/test_place.py
+++ openatlas/tests/test_place.py
@@ -5,5 +5,20 @@ from openatlas.test_base import TestBaseCase
 class PlaceTest(TestBaseCase):
 
     def test_place(self):
+        rv = self.app.get('/place/insert/E18')
+        assert '+ Place' in rv.data
+        form_data = {'name': 'Test place'}
+        rv = self.app.post('/place/insert/E18', data=form_data)
+        place_id = rv.location.split('/')[-1]
+        form_data['continue_'] = 'yes'
+        rv = self.app.post('/place/insert/E21', data=form_data, follow_redirects=True)
+        assert 'Entity created' in rv.data
         rv = self.app.get('/place')
-        #assert 'Overview' in rv.data
+        assert 'Test place' in rv.data
+        rv = self.app.get('/place/update/' + place_id)
+        assert 'Test place' in rv.data
+        form_data['name'] = 'Test place updated'
+        rv = self.app.post('/place/update/' + place_id, data=form_data, follow_redirects=True)
+        assert 'Test place updated' in rv.data
+        rv = self.app.get('/place/delete/' + place_id, follow_redirects=True)
+        assert 'Entity deleted' in rv.data
