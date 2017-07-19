f96980a09f58f7845ad714352fc46b92dd6c48b8
Alexander Watzinger <alexander.watzinger@craws.net>
2017-07-19 23:15:50 +0200

    user insert and view

--- openatlas/models/user.py
+++ openatlas/models/user.py
@@ -1,9 +1,12 @@
 # Copyright 2017 by Alexander Watzinger and others. Please see the file README.md for licensing information
+import bcrypt
+
 import openatlas
 
 
 class User(object):
     def __init__(self, row):
+        self.id = row.id
         self.active = True if row.active == 1 else False
         self.username = row.username
         self.password = row.password
@@ -12,7 +15,7 @@ class User(object):
         self.login_failed_count = row.login_failed_count
         self.real_name = row.real_name
         self.email = row.email
-        self.info = row.info
+        self.description = row.info
         self.settings = []
         self.bookmarks = []
         self.password_reset_code = row.password_reset_code
@@ -39,3 +42,29 @@ class UserMapper(object):
             users.append(User(row))
         openatlas.debug_model['user'] += 1
         return users
+
+    @staticmethod
+    def get_by_id(user_id):
+        cursor = openatlas.get_cursor()
+        cursor.execute(UserMapper.sql + ' WHERE u.id = %(id)s;', {'id': user_id})
+        return User(cursor.fetchone())
+
+    @staticmethod
+    def insert(form):
+        cursor = openatlas.get_cursor()
+        sql = '''INSERT INTO web.user (username, real_name, info, email, active, password, group_id) VALUES
+            (%(username)s, %(name)s, %(info)s, %(email)s, %(active)s, %(password)s, %(group_id)s) RETURNING id;'''
+        cursor.execute(sql, {
+            'username': form.username.data,
+            'name': form.name.data,
+            'info': form.description.data,
+            'email': form.email.data,
+            'active': form.active.data,
+            'group_id': 1,
+            'password': bcrypt.hashpw(form.password.data, bcrypt.gensalt(12))})
+        return cursor.fetchone()[0]
+
+    @staticmethod
+    def delete(user_id):
+        sql = 'DELETE FROM web."user" WHERE id = %(user_id)s;'
+        openatlas.get_cursor().execute(sql, {'user_id': user_id})
--- openatlas/views/user.py
+++ openatlas/views/user.py
@@ -1,7 +1,8 @@
 # Copyright 2017 by Alexander Watzinger and others. Please see the file README.md for licensing information
 from flask_babel import lazy_gettext as _
-from flask import render_template
+from flask import render_template, flash, url_for
 from flask_wtf import Form
+from werkzeug.utils import redirect
 from wtforms import StringField, TextAreaField, HiddenField, PasswordField, BooleanField
 from wtforms.validators import InputRequired, Email
 from openatlas import app
@@ -19,6 +20,12 @@ class UserForm(Form):
     email = StringField(uc_first(_('email')), validators=[InputRequired(), Email()])
     send_info = BooleanField(_('send account information'))
     continue_ = HiddenField()
+
+
+@app.route('/user/view/<int:user_id>')
+def user_view(user_id):
+    user = UserMapper.get_by_id(user_id)
+    return render_template('user/view.html', user=user)
 
 
 @app.route('/user')
@@ -40,7 +47,24 @@ def user_index():
     return render_template('user/index.html', tables=tables)
 
 
+@app.route('/user/update/<int:user_id>', methods=['POST', 'GET'])
+def user_update():
+    pass
+
+
 @app.route('/user/insert', methods=['POST', 'GET'])
 def user_insert():
     form = UserForm()
+    form.active.data = True
+    if form.validate_on_submit():
+        user_id = UserMapper.insert(form)
+        flash(_('user created'), 'success')
+        return redirect(url_for('user_view', user_id=user_id))
     return render_template('user/insert.html', form=form)
+
+
+@app.route('/admin/user/delete/<int:user_id>')
+def user_delete(user_id):
+    UserMapper.delete(user_id)
+    flash(_('user deleted'), 'success')
+    return redirect(url_for('user_index'))
