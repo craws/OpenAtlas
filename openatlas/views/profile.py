29cc58cafdfec1f24e23666f42f85f4d76780a0a
Alexander Watzinger <alexander.watzinger@craws.net>
2017-08-17 17:05:33 +0200

    user profile

--- openatlas/views/profile.py
+++ openatlas/views/profile.py
@@ -2,20 +2,42 @@
 from flask import render_template
 from flask_babel import lazy_gettext as _
 from flask_login import login_required, current_user
+from flask_wtf import Form
+from wtforms import SelectField
+
+import openatlas
 from openatlas import app
+from openatlas.util.util import uc_first
+
+
+class ProfileForm(Form):
+    language = SelectField(uc_first(_('language')), choices=[])
+    # theme = SelectField(uc_first(_('theme')), choices=[])
+    layout = SelectField(uc_first(_('layout')), choices=[
+        ('default', uc_first(_('default'))), ('advanced', uc_first(_('advanced')))])
+    table_rows = SelectField(uc_first(_('table rows')), choices=[])
 
 
 @app.route('/profile')
 @login_required
 def profile_index():
-    tables = {'info': [
+    data = {'info': [
         (_('username'), current_user.username),
         (_('name'), current_user.real_name),
         (_('email'), current_user.email),
         (_('show email'), current_user.get_setting('show_email')),
-        (_('newsletter'), current_user.get_setting('newsletter')),
-    ]}
-    return render_template('profile/index.html', tables=tables)
+        (_('newsletter'), current_user.get_setting('newsletter'))]}
+
+    form = ProfileForm()
+    getattr(form, 'language').choices = openatlas.app.config['LANGUAGES'].items()
+    getattr(form, 'table_rows').choices = openatlas.default_table_rows.items()
+    data['display'] = [
+        (form.language.label, form.language),
+        # (form.theme.label, form.theme),
+        (form.layout.label, form.layout),
+        (form.table_rows.label, form.table_rows)]
+
+    return render_template('profile/index.html', data=data, form=form)
 
 
 @app.route('/profile/update')
