58e155de36a984d4cbc10ac2cbb11561ac607427
Alexander Watzinger <alexander.watzinger@craws.net>
2017-11-07 17:58:10 +0100

    Tests for nodes

--- openatlas/tests/test_node.py
+++ openatlas/tests/test_node.py
@@ -1,7 +1,7 @@
 # Copyright 2017 by Alexander Watzinger and others. Please see README.md for licensing information
 from flask import url_for
 
-from openatlas import app
+from openatlas import app, NodeMapper
 from openatlas.test_base import TestBaseCase
 
 
@@ -10,5 +10,40 @@ class NodeTest(TestBaseCase):
     def test_node(self):
         self.login()
         with app.app_context():
+            actor_node = NodeMapper.get_hierarchy_by_name('Actor Actor Relation')
             rv = self.app.get(url_for('node_index'))
             assert b'Actor Actor Relation' in rv.data
+            rv = self.app.post(
+                url_for('node_insert', root_id=actor_node.id), data={'name_search': 'new'})
+            assert b'Inverse' in rv.data
+            form_data = {
+                'name': 'My secret node',
+                'name_inverse': 'Do I look inverse?',
+                'description': 'Very important!'}
+            rv = self.app.post(url_for('node_insert', root_id=actor_node.id), data=form_data)
+            node_id = rv.location.split('/')[-1].replace('node#tab-', '')
+            rv = self.app.get(url_for('node_update', id_=node_id))
+            assert b'My secret node' in rv.data
+            rv = self.app.post(
+                url_for('node_update', id_=node_id),
+                data=form_data,
+                follow_redirects=True)
+            assert b'Changes have been saved.' in rv.data
+            # test forbidden system node
+            rv = self.app.post(
+                url_for('node_update', id_=actor_node.id),
+                data=form_data,
+                follow_redirects=True)
+            assert b'Forbidden' in rv.data
+            # test update with self as root
+            form_data[str(actor_node.id)] = node_id
+            rv = self.app.post(
+                url_for('node_update', id_=node_id),
+                data=form_data,
+                follow_redirects=True)
+            assert b'super' in rv.data
+            #  test delete system node
+            rv = self.app.get(url_for('node_delete', id_=actor_node.id), follow_redirects=True)
+            assert b'Forbidden' in rv.data
+            rv = self.app.get(url_for('node_delete', id_=node_id), follow_redirects=True)
+            assert b'The entry has been deleted.' in rv.data
