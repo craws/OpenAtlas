5d7ff6635f7214b2ab4eabaf3ff51b96fa86abe1
Alexander Watzinger <alexander.watzinger@craws.net>
2018-05-02 16:22:18 +0200

    Update changelog

--- openatlas/util/changelog.py
+++ openatlas/util/changelog.py
@@ -5,6 +5,11 @@ from collections import OrderedDict
 class Changelog:
     """ A big OrderedDict with changelog data, maybe not the best place and format to keep it."""
     versions = OrderedDict()
+    versions['3.5.0'] = {
+        'date': 'TBA',
+        'data': OrderedDict(
+            feature=OrderedDict([
+                ('896', 'Legal notice text option')]))}
     versions['3.4.0'] = {
         'date': '2018-04-17',
         'data': OrderedDict(
