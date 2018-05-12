0bd1b714db7b70691dff9f3f4453d8e04362608b
Alexander Watzinger <alexander.watzinger@craws.net>
2018-05-12 17:17:31 +0200

    Update SQL fix

--- install/upgrade/3.5.0.sql
+++ install/upgrade/3.5.0.sql
@@ -3,6 +3,6 @@
 BEGIN;
 
 -- Login mails where removed, so also removing setting for it
-DELETE FROM settings WHERE NAME = 'mail_recipients_login';
+DELETE FROM web.settings WHERE NAME = 'mail_recipients_login';
 
 COMMIT;
