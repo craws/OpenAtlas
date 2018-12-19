9bb9c1575fc4627cf3b25a0e122e522e4e80b08c
Alexander Watzinger <alexander.watzinger@craws.net>
2018-12-19 17:49:56 +0100

    Added profile image table to upgrade sql

--- install/upgrade/3.12.0.sql
+++ install/upgrade/3.12.0.sql
@@ -9,4 +9,26 @@ INSERT INTO web.settings (name, value) VALUES ('minimum_tablesorter_search', '1'
 -- Fix possible naming errors at gis point
 UPDATE gis.point SET type = 'centerpoint';
 
+-- Profile images
+CREATE TABLE web.entity_profile_image (
+    id integer NOT NULL,
+    entity_id integer NOT NULL,
+    image_id integer NOT NULL,
+    created timestamp without time zone DEFAULT now() NOT NULL
+);
+ALTER TABLE web.entity_profile_image OWNER TO openatlas;
+CREATE SEQUENCE web.entity_profile_image_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+ALTER TABLE web.entity_profile_image_id_seq OWNER TO openatlas;
+ALTER SEQUENCE web.entity_profile_image_id_seq OWNED BY web.entity_profile_image.id;
+ALTER TABLE ONLY web.entity_profile_image ALTER COLUMN id SET DEFAULT nextval('web.entity_profile_image_id_seq'::regclass);
+ALTER TABLE ONLY web.entity_profile_image ADD CONSTRAINT entity_profile_image_entity_id_image_id_key UNIQUE (entity_id, image_id);
+ALTER TABLE ONLY web.entity_profile_image ADD CONSTRAINT entity_profile_image_pkey PRIMARY KEY (id);
+ALTER TABLE ONLY web.entity_profile_image ADD CONSTRAINT entity_profile_image_entity_id_fkey FOREIGN KEY (entity_id) REFERENCES model.entity(id) ON UPDATE CASCADE ON DELETE CASCADE;
+ALTER TABLE ONLY web.entity_profile_image ADD CONSTRAINT entity_profile_image_image_id_fkey FOREIGN KEY (image_id) REFERENCES model.entity(id) ON UPDATE CASCADE ON DELETE CASCADE;
+
 COMMIT;
