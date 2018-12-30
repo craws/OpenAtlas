e0d4d1013c122ee82d9c6a7610b21ee684cb5399
Alexander Watzinger <alexander.watzinger@craws.net>
2018-12-30 22:28:10 +0100

    Refactoring network

--- openatlas/models/network.py
+++ openatlas/models/network.py
@@ -14,6 +14,8 @@ class Network:
         classes = [code for code, param in params['classes'].items() if param['active']]
         if not classes:
             return None  # Return nothing if no classes are selected
+
+        # Get edges
         entities = set()
         edges = ''
         if properties:
@@ -29,6 +31,8 @@ class Network:
                 edges += "{{'source':'{domain_id}','target':'{range_id}'}},".format(
                     domain_id=row.domain_id, range_id=row.range_id)
                 entities.update([row.domain_id, row.range_id])
+
+        # Get entities
         sql = "SELECT id, class_code, name FROM model.entity WHERE class_code IN %(classes)s;"
         g.cursor.execute(sql, {'classes': tuple(classes)})
         debug_model['div sql'] += 1
@@ -36,14 +40,12 @@ class Network:
         entities_already = set()
         for row in g.cursor.fetchall():
             if params['options']['orphans'] or row.id in entities:
-                name = row.name.replace("'", "").replace('Location of ', '').replace('\n', ' ')\
-                    .replace('\r', ' ')
                 entities_already.add(row.id)
                 nodes += """{{'id':'{id}','name':'{name}','color':'{color}'}},""".format(
-                    id=row.id, name=truncate_string(name, span=False),
+                    id=row.id, name=truncate_string(row.name, span=False),
                     color=params['classes'][row.class_code]['color'])
 
-        # Get elements of links which weren't present in class selection
+        # Get entities of links which weren't present in class selection
         array_diff = [item for item in entities if item not in entities_already]
         if array_diff:
             sql = "SELECT id, class_code, name FROM model.entity WHERE id IN %(array_diff)s;"
@@ -54,8 +56,7 @@ class Network:
                 color = ''
                 if row.class_code in params['classes']:  # pragma: no cover
                     color = params['classes'][row.class_code]['color']
-                name = row.name.replace("'", "").replace('Location of ', '').replace('\n', ' ')\
-                    .replace('\r', ' ')
                 nodes += """{{'id':'{id}','name':'{name}','color':'{color}'}},""".format(
-                    id=row.id, name=truncate_string(name, span=False), color=color)
+                    id=row.id, name=truncate_string(row.name, span=False), color=color)
+
         return "graph = {'nodes': [" + nodes + "],  links: [" + edges + "]};" if nodes else None
