a371ead054fc75b2de9aa71c5ad8c4cf3425efa9
Alexander Watzinger <alexander.watzinger@craws.net>
2019-01-22 19:35:25 +0100

    Update upgrade sql

--- install/upgrade/3.13.0.sql
+++ install/upgrade/3.13.0.sql
@@ -14,19 +14,19 @@ ALTER TABLE model.entity ADD COLUMN end_comment text;
 ALTER TABLE model.link ADD COLUMN begin_from timestamp without time zone;
 ALTER TABLE model.link ADD COLUMN begin_to timestamp without time zone;
 ALTER TABLE model.link ADD COLUMN begin_comment text;
-ALTER TABLE model.link ADD COLUMN nd_from timestamp without time zone;
+ALTER TABLE model.link ADD COLUMN end_from timestamp without time zone;
 ALTER TABLE model.link ADD COLUMN end_to timestamp without time zone;
 ALTER TABLE model.link ADD COLUMN end_comment text;
 
-DROP FUNCTION IF EXISTS model.delete_entity_related();
+DROP FUNCTION IF EXISTS model.delete_entity_related() CASCADE;
 CREATE FUNCTION model.delete_entity_related() RETURNS trigger
     LANGUAGE plpgsql
     AS $$
         BEGIN
-            -- Delete dates (OA1, OA2, OA3, OA4, OA5, OA6) and aliases (P1, P131)
+            -- Delete aliases (P1, P131)
             IF OLD.class_code IN ('E6', 'E7', 'E8', 'E12', 'E18', 'E21', 'E22', 'E40', 'E74') THEN
                 DELETE FROM model.entity WHERE id IN (
-                    SELECT range_id FROM model.link WHERE domain_id = OLD.id AND property_code IN ('OA1', 'OA2', 'OA3', 'OA4', 'OA5', 'OA6', 'P1', 'P131'));
+                    SELECT range_id FROM model.link WHERE domain_id = OLD.id AND property_code IN ('P1', 'P131'));
             END IF;
 
             -- Delete location (E53) if it was a place or find
@@ -48,6 +48,6 @@ CREATE TRIGGER on_delete_entity BEFORE DELETE ON model.entity FOR EACH ROW EXECU
 -- Drop obsolete fields
 ALTER TABLE model.entity DROP COLUMN value_integer;
 ALTER TABLE model.entity DROP COLUMN value_timestamp;
-DELETE FROM model.classes WHERE code IN ('OA1', 'OA2', 'OA3', 'OA4', 'OA5', 'OA6');
+DELETE FROM model.property WHERE code IN ('OA1', 'OA2', 'OA3', 'OA4', 'OA5', 'OA6');
 
 COMMIT;
