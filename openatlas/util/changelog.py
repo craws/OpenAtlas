dae685892672e5b118f0963b3abc029b6d0c5934
Alexander Watzinger <alexander.watzinger@craws.net>
2019-07-07 14:53:39 +0200

    Update changelog

--- openatlas/util/changelog.py
+++ openatlas/util/changelog.py
@@ -4,13 +4,11 @@ from collections import OrderedDict
 
 class Changelog:
     versions = OrderedDict()  # type: OrderedDict
-    versions['3.18.0'] = ['TBA', OrderedDict([
+    versions['3.18.0'] = ['2019-07-07', OrderedDict([
         ('feature', OrderedDict([
             ('1036', 'Search for similar names'),
             ('1034', 'Advanced data integrity check functions'),
-            ('1025', 'New OpenAtlas project site')
-        ])),
-        ('fix', OrderedDict([]))])]
+            ('1025', 'New OpenAtlas project site')]))])]
     versions['3.17.1'] = ['2019-05-21', OrderedDict([
         ('fix', OrderedDict([
             ('1033', 'Map editing breaks upon save w/o edit')]))])]
