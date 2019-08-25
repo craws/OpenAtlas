63cc9a5287ec5e78d55d2767aec2d3a9ae0e2d61
Alexander Watzinger <alexander.watzinger@craws.net>
2019-08-25 15:20:32 +0200

    Update upgrade SQL

--- install/upgrade/3.19.0.sql
+++ install/upgrade/3.19.0.sql
@@ -3,20 +3,23 @@
 
 BEGIN;
 
--- Add new user group contributer
+-- Add new user group contributor - Issue #1040
 INSERT INTO web.group (name) VALUES ('contributor');
 
--- Remove settings for obsolete color themes
+-- Remove settings for obsolete color themes - Issue #1041
 DELETE FROM web.user_settings WHERE name = 'theme';
 
--- Insert additional boolean field "locked" to web.hierarchy
+-- Remap Production (E12) and Destruction (E6) to Activity (E7) - Issue #1054
+UPDATE model.entity SET class_code = 'E7' WHERE class_code in ('E12', 'E6');
+
+-- Insert additional boolean field "locked" to web.hierarchy for e.g. GeoNames - Issue #928
 ALTER TABLE web.hierarchy ADD COLUMN locked boolean;
 COMMENT ON COLUMN web.hierarchy.locked IS 'True if these types are not editable';
 ALTER TABLE web.hierarchy ALTER column locked SET DEFAULT false;
 UPDATE web.hierarchy SET locked = false;
 ALTER TABLE web.hierarchy ALTER column locked SET NOT NULL;
 
--- Types for external references e.g. GeoNames
+-- Types for external references e.g. GeoNames - Issue #928
 INSERT INTO model.entity (class_code, name, description) VALUES ('E55', 'External Reference Match', 'SKOS based definition of the confidence degree that concepts can be used interchangeable.');
 INSERT INTO model.entity (class_code, name, description) VALUES ('E55', 'exact match', 'High degree of confidence that the concepts can be used interchangeably.');
 INSERT INTO model.entity (class_code, name, description) VALUES ('E55', 'close match', 'Concepts are sufficiently similar that they can be used interchangeably in some information retrieval applications.');
@@ -25,7 +28,7 @@ INSERT INTO model.link (property_code, range_id, domain_id) VALUES
     ('P127', (SELECT id FROM model.entity WHERE name='External Reference Match'), (SELECT id FROM model.entity WHERE name='close match'));
 INSERT INTO web.hierarchy (id, name, multiple, system, directional, value_type, locked) VALUES ((SELECT id FROM model.entity WHERE name='External Reference Match'), 'External Reference Match', False, True, False, False, True);
 
--- Add user private notes
+-- Add user private notes - Issue #1042
 CREATE TABLE web.user_notes (
     id integer NOT NULL,
     user_id integer NOT NULL,
