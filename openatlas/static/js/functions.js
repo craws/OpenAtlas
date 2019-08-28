bfa8f658dff8e8884cfcbfe9ba727de80121ef3a
Alexander Watzinger <alexander.watzinger@craws.net>
2019-08-28 16:13:14 +0200

    Checkbox and similar search for DataTables

--- openatlas/static/js/functions.js
+++ openatlas/static/js/functions.js
@@ -13,6 +13,43 @@ tinymce.init({
 
 $(document).ready(function() {
 
+    // DataTables - sort for checkbox columns
+    $.fn.dataTable.ext.order['dom-checkbox'] = function(settings, col) {
+        return this.api().column(col, {order:'index'}).nodes().map( function (td, i) {
+            return $('input', td).prop('checked') ? '1' : '0';
+        });
+    };
+
+    // DataTables - ignore special characters for search
+    (function(){
+        function removeAccents ( data ) {
+            if ( data.normalize ) {
+                // Use I18n API if avaiable to split characters and accents, then remove
+                // the accents wholesale. Note that we use the original data as well as
+                // the new to allow for searching of either form.
+                return data +' '+ data
+                    .normalize('NFD')
+                    .replace(/[\u0300-\u036f]/g, '');
+            }
+            return data;
+        }
+        var searchType = jQuery.fn.DataTable.ext.type.search;
+        searchType.string = function ( data ) {
+            return ! data ?
+                '' :
+                typeof data === 'string' ?
+                    removeAccents( data ) :
+                    data;
+        };
+        searchType.html = function ( data ) {
+            return ! data ?
+                '' :
+                typeof data === 'string' ?
+                    removeAccents( data.replace( /<.*?>/g, '' ) ) :
+                    data;
+        };
+    }());
+
     /* jQuery UI tabs init */
     $("#tabs").tabs({
         activate: function(event, ui) {
