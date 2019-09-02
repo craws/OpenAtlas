f74d96bb9ca5c7718f4f52ed6f7bce7f6f5ab271
Alexander Watzinger <alexander.watzinger@craws.net>
2019-09-02 22:30:14 +0200

    Update changelog

--- install/upgrade/3.20.0.sql
+++ install/upgrade/3.20.0.sql
@@ -3,7 +3,7 @@
 
 BEGIN;
 
--- Adaptions for DataTables
+--  #1043 DataTables - Adaptions for DataTables
 UPDATE web.settings SET value = '25' WHERE name= 'default_table_rows' AND value = '20';
 UPDATE web.user_settings SET value = '25' WHERE name= 'table_rows' AND value = '20';
 
