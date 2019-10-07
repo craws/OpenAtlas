34a889930393ba488f8136f6f6d58403c7032600
Alexander Watzinger <alexander.watzinger@craws.net>
2019-10-07 17:57:52 +0200

    Raised version

--- config/default.py
+++ config/default.py
@@ -4,7 +4,7 @@ from collections import OrderedDict
 
 from flask_babel import lazy_gettext as _
 
-VERSION = '3.20.0'
+VERSION = '3.21.0'
 DEMO_MODE = False  # If in demo mode some options are disabled and the login form is pre filled
 
 LANGUAGES = {'en': 'English', 'de': 'Deutsch'}
