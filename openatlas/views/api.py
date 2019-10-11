0b02cf477b893fa57099572a6eac28d61bfb9baf
BernhardKoschicek <b.koschicek@gmail.com>
2019-10-11 17:12:08 +0200

    Return basic json

--- openatlas/views/api.py
+++ openatlas/views/api.py
@@ -1,29 +1,19 @@
 # Created by Alexander Watzinger and others. Please see README.md for licensing information
-import ast
 
-from flask import flash, g, render_template, request, url_for
-from flask_babel import lazy_gettext as _
-from flask_wtf import Form
-from werkzeug.utils import redirect
-from wtforms import HiddenField, StringField, SubmitField, TextAreaField
-from wtforms.validators import InputRequired
 
-from openatlas import app, logger
-from openatlas.forms.forms import DateForm, TableField, TableMultiField, build_form
+from flask import render_template, json
+
+from openatlas import app
 from openatlas.models.entity import EntityMapper
-from openatlas.models.link import LinkMapper
-from openatlas.models.user import UserMapper
-from openatlas.util.table import Table
-from openatlas.util.util import (display_remove_link, get_base_table_data, get_entity_data,
-                                 get_profile_image_table_link, is_authorized, link, required_group,
-                                 truncate_string, uc_first, was_modified)
+from openatlas.util.util import required_group
 
 
 @app.route('/api/<version>/entity/<int:id_>')
 @required_group('manager')
 def api_entity(version: str, id_: int) -> str:
-
-    return render_template('api/entity.html', version=version, id_=id_)
+    entity = EntityMapper.get_by_id(id_)
+    data = {"Entity": {'id': entity.id, 'name': entity.name}}
+    return json.dumps(data)
 
 
 @app.route('/api')
