3f4557bc5ec39fd15a742668d4a91534f0e128bd
Alexander Watzinger <alexander.watzinger@craws.net>
2019-12-04 17:19:34 +0100

    CRM - import new classes

--- install/crm/cidoc_rtfs_parser.py
+++ install/crm/cidoc_rtfs_parser.py
@@ -11,9 +11,9 @@
 # Todo: Add and document shortcuts
 
 import time
-from pprint import pprint
 
 import psycopg2.extras
+from rdflib import URIRef
 from rdflib.graph import Graph
 
 FILENAME = 'cidoc_crm_v6.2.1.rdfs'
@@ -47,36 +47,59 @@ class Item:
 
 def import_cidoc():  # pragma: no cover
     start = time.time()
-    classes = []
-    properties = []
-
+    classes = {}
+    properties = {}
     graph = Graph()
     graph.parse(FILENAME, format='application/rdf+xml')
-    # pprint(dir(graph))
-    for subject, predicate, object_ in graph:
 
+    # Get classes and properties
+    for subject, predicate, object_ in graph:
         code, name = subject.replace(CRM_URL, '').split('_', 1)
         item = Item(code, name.replace('_', ' '), graph.comment(subject))
-        for language in ['de', 'en', 'fr', 'ru', 'el', 'pt', 'zh']:
-            translation = graph.preferredLabel(subject, lang=language)[0]
-            if translation:
-                item.label[language] = translation[1]
-        if name[0] == 'E':
-            classes.append(item)
+        # for language in ['de', 'en', 'fr', 'ru', 'el', 'pt', 'zh']:
+        #    translation = graph.preferredLabel(subject, lang=language)
+        #    if translation:
+        #        item.label[language] = translation[0][1]
+        if code[0] == 'E':
+            classes[code] = item
         elif name[0] == 'P':
-            properties.append(item)
-
-        # print(item.code)
-        # print(item.name)
-        # print(item.comment)
+            properties[code] = item
         # print(item.label['en'])
         # print(item.label['de'])
-        break
 
-    # connection = connect()
-    # cursor = connection.cursor(cursor_factory=psycopg2.extras.NamedTupleCursor)
-    # cursor.execute('BEGIN;')
-    # cursor.execute('COMMIT;')
+    # Get all subClassOf
+    # sub_class_triples = graph.triples((None,
+    #                                   URIRef('http://www.w3.org/2000/01/rdf-schema#subClassOf'),
+    #                                   None))
+    # for subject__, predicate__, object__ in sub_class_triples:
+        # pprint(subject__.replace(CRM_URL, '').split('_', 1)[0])
+        # pprint(object__.replace(CRM_URL, '').split('_', 1)[0])
+
+    connection = connect()
+    cursor = connection.cursor(cursor_factory=psycopg2.extras.NamedTupleCursor)
+    cursor.execute('BEGIN;')
+    cursor.execute("""
+        ALTER TABLE IF EXISTS ONLY model.entity DROP CONSTRAINT IF EXISTS entity_class_code_fkey;
+        ALTER TABLE IF EXISTS ONLY model.class_inheritance DROP CONSTRAINT IF EXISTS class_inheritance_super_code_fkey;
+        ALTER TABLE IF EXISTS ONLY model.class_inheritance DROP CONSTRAINT IF EXISTS class_inheritance_sub_code_fkey;
+        ALTER TABLE IF EXISTS ONLY model.class_i18n DROP CONSTRAINT IF EXISTS class_i18n_class_code_fkey;
+        ALTER TABLE IF EXISTS ONLY model.property DROP CONSTRAINT IF EXISTS property_range_class_code_fkey;
+        ALTER TABLE IF EXISTS ONLY model.property_inheritance DROP CONSTRAINT IF EXISTS property_inheritance_super_code_fkey;
+        ALTER TABLE IF EXISTS ONLY model.property_inheritance DROP CONSTRAINT IF EXISTS property_inheritance_sub_code_fkey;
+        ALTER TABLE IF EXISTS ONLY model.property_i18n DROP CONSTRAINT IF EXISTS property_i18n_property_code_fkey;
+        ALTER TABLE IF EXISTS ONLY model.property DROP CONSTRAINT IF EXISTS property_domain_class_code_fkey;
+        TRUNCATE model.class_inheritance, model.class_i18n, model.class, model.property_inheritance,
+            model.property_i18n;""")
+
+    for code, class_ in classes.items():
+        print(class_.code)
+        print(class_.name)
+        print(class_.comment)
+        sql = 'INSERT INTO model.class (code, name, comment) VALUES (%(code)s, %(name)s, %(comment)s);'
+        cursor.execute(sql, {'code': class_.code, 'name': class_.name, 'comment': class_.comment})
+
+    cursor.execute('COMMIT;')
+
     print('Execution time: ' + str(int(time.time() - start)) + ' seconds')
 
 
