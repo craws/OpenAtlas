1d233a1038600d64ebde22444f0d6bd8af650609
Alexander Watzinger <alexander.watzinger@craws.net>
2019-12-08 18:03:22 +0100

    CRM upgrade: i18n and subClassOf for classes

--- install/crm/cidoc_rtfs_parser.py
+++ install/crm/cidoc_rtfs_parser.py
@@ -1,5 +1,3 @@
-# Created by Alexander Watzinger and others. Please see README.md for licensing information
-
 # This is script is for developing purposes and is not needed to install OpenAtlas.
 # CIDOC CRM is used as basis for the underlying data model of OpenAtlas.
 # Currently we are using CIDOC CRM v6.2.1
@@ -7,8 +5,6 @@
 
 # In this script the rdfs is parsed and entered to a PostgreSQL database
 # apt-get install python3-rdflib
-
-# Todo: Add and document shortcuts
 
 import time
 
@@ -38,11 +34,12 @@ def connect():
 
 
 class Item:
-    def __init__(self, code, name, comment) -> None:
+    def __init__(self, code: str, name: str, comment: str) -> None:
         self.code = code
         self.name = name
         self.comment = comment
         self.label: dict = {}
+        self.sub_class_of: list = []
 
 
 def import_cidoc():  # pragma: no cover
@@ -56,31 +53,33 @@ def import_cidoc():  # pragma: no cover
     for subject, predicate, object_ in graph:
         code, name = subject.replace(CRM_URL, '').split('_', 1)
         item = Item(code, name.replace('_', ' '), graph.comment(subject))
+
+        # Translations
         for language in ['de', 'en', 'fr', 'ru', 'el', 'pt', 'zh']:
             translation = graph.preferredLabel(subject, lang=language)
+            if translation and translation[0][1]:
+                item.label[language] = translation[0][1]
 
-            if translation:
-                print(translation[0])
-                item.label[language] = translation
         if code[0] == 'E':
             classes[code] = item
         elif name[0] == 'P':
             properties[code] = item
-        # print(item.label['en'])
-        # print(item.label['de'])
 
-    # Get all subClassOf
-    # sub_class_triples = graph.triples((None,
-    #                                   URIRef('http://www.w3.org/2000/01/rdf-schema#subClassOf'),
-    #                                   None))
-    # for subject__, predicate__, object__ in sub_class_triples:
-        # pprint(subject__.replace(CRM_URL, '').split('_', 1)[0])
-        # pprint(object__.replace(CRM_URL, '').split('_', 1)[0])
+    # Get subClassOf
+    sub_class_triples = graph.triples((None,
+                                       URIRef('http://www.w3.org/2000/01/rdf-schema#subClassOf'),
+                                       None))
+    for subject__, predicate__, object__ in sub_class_triples:
+        class_ = subject__.replace(CRM_URL, '').split('_', 1)[0]
+        sub_class_of = object__.replace(CRM_URL, '').split('_', 1)[0]
+        classes[class_].sub_class_of.append(sub_class_of)
 
     connection = connect()
     cursor = connection.cursor(cursor_factory=psycopg2.extras.NamedTupleCursor)
-    cursor.execute('BEGIN;')
     cursor.execute("""
+        BEGIN;
+        ALTER TABLE model.class DROP COLUMN IF EXISTS comment;
+        ALTER TABLE model.class ADD COLUMN comment text;
         ALTER TABLE model.entity DROP CONSTRAINT IF EXISTS entity_class_code_fkey;
         ALTER TABLE model.class_inheritance DROP CONSTRAINT IF EXISTS class_inheritance_super_code_fkey;
         ALTER TABLE model.class_inheritance DROP CONSTRAINT IF EXISTS class_inheritance_sub_code_fkey;
@@ -92,19 +91,35 @@ def import_cidoc():  # pragma: no cover
         ALTER TABLE model.property DROP CONSTRAINT IF EXISTS property_domain_class_code_fkey;
         ALTER TABLE model.class_i18n DROP COLUMN IF EXISTS attribute;
         ALTER TABLE model.property_i18n DROP COLUMN IF EXISTS attribute;
-        TRUNCATE model.class_inheritance, model.class_i18n, model.class, model.property_inheritance, model.property_i18n;""")
+        TRUNCATE model.class_inheritance, model.class_i18n, model.class, model.property_inheritance, model.property_i18n;
+        ALTER SEQUENCE model.class_id_seq RESTART;
+        ALTER SEQUENCE model.class_inheritance_id_seq RESTART;
+        ALTER SEQUENCE model.class_i18n_id_seq RESTART;
+        ALTER SEQUENCE model.property_id_seq RESTART;
+        ALTER SEQUENCE model.property_inheritance_id_seq RESTART;
+        ALTER SEQUENCE model.property_i18n_id_seq RESTART;
+
+        """)
 
     for code, class_ in classes.items():
         sql = 'INSERT INTO model.class (code, name, comment) VALUES (%(code)s, %(name)s, %(comment)s);'
         cursor.execute(sql, {'code': class_.code, 'name': class_.name, 'comment': class_.comment})
     for code, class_ in classes.items():
-        for language, label in class_.label:
+        for sub_code_of in class_.sub_class_of:
+            sql = 'INSERT INTO model.class_inheritance (super_code, sub_code) VALUES (%(super_code)s, %(sub_code)s);'
+            cursor.execute(sql, {'super_code': sub_code_of, 'sub_code': class_.code})
+    for code, class_ in classes.items():
+        for language, label in class_.label.items():
             sql = """
                 INSERT INTO model.class_i18n (class_code, language_code, text)
                 VALUES (%(class)s, %(language)s, %(text)s);"""
             cursor.execute(sql, {'class': class_.code, 'language': language, 'text': label})
 
-    cursor.execute('COMMIT;')
+    cursor.execute("""
+        ALTER TABLE ONLY model.class_i18n ADD CONSTRAINT class_i18n_class_code_fkey FOREIGN KEY (class_code) REFERENCES model.class(code) ON UPDATE CASCADE ON DELETE CASCADE;
+        ALTER TABLE ONLY model.class_inheritance ADD CONSTRAINT class_inheritance_sub_code_fkey FOREIGN KEY (sub_code) REFERENCES model.class(code) ON UPDATE CASCADE ON DELETE CASCADE;
+        ALTER TABLE ONLY model.class_inheritance ADD CONSTRAINT class_inheritance_super_code_fkey FOREIGN KEY (super_code) REFERENCES model.class(code) ON UPDATE CASCADE ON DELETE CASCADE;
+        COMMIT;""")
 
     print('Execution time: ' + str(int(time.time() - start)) + ' seconds')
 
