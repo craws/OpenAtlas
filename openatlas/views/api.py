d4e24d374c2d1d5dd1888a0c6195ea35ed71a99c
Alexander Watzinger <alexander.watzinger@craws.net>
2020-02-15 18:00:04 +0100

    Changed API access to readonly

--- openatlas/views/api.py
+++ openatlas/views/api.py
@@ -6,25 +6,25 @@ from openatlas.util.util import required_group
 
 
 @app.route('/api/0.1/entity/<int:id_>')
-@required_group('manager')
+@required_group('readonly')
 def api_entity(id_: int) -> Response:
     return Response(json.dumps(Api.get_entity(id_=id_)), mimetype='application/ld+json')
 
 
 @app.route('/api/0.1')
-@required_group('manager')
+@required_group('readonly')
 def api_get_multiple_entities():
     entity = request.args.getlist('entity')
     return Response(json.dumps(Api.get_entities_by_id(ids=entity)), mimetype='application/ld+json')
 
 
 @app.route('/api/0.1/<code>')
-@required_group('manager')
+@required_group('readonly')
 def api_collection(code):
     return Response(json.dumps(Api.get_entities_by_code(code_=code)), mimetype='application/ld+json')
 
 
 @app.route('/api')
-@required_group('manager')
+@required_group('readonly')
 def api_index() -> str:
     return render_template('api/index.html')
