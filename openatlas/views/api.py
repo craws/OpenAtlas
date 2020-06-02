2e9c123a2e54725df00e9b8f810ff4cae9f12314
BernhardKoschicek <b.koschicek@gmail.com>
2020-06-02 16:23:23 +0200

    API: change methods to only GET

--- openatlas/views/api.py
+++ openatlas/views/api.py
@@ -10,13 +10,13 @@ from openatlas.util.util import api_access
 # Todo: unit tests and Mypy checks
 
 
-@app.route('/api/0.1/entity/<int:id_>')
+@app.route('/api/0.1/entity/<int:id_>', methods=['GET'])
 @api_access()  # type: ignore
 def api_entity(id_: int) -> Response:
     return jsonify(Api.get_entity(id_=id_))
 
 
-@app.route('/api/0.1/entity/download/<int:id_>')
+@app.route('/api/0.1/entity/download/<int:id_>', methods=['GET'])
 @api_access()  # type: ignore
 def api_download_entity(id_: int) -> Response:
     # flash(_('Download successful'), 'info')
@@ -25,26 +25,26 @@ def api_download_entity(id_: int) -> Response:
                     headers={'Content-Disposition': 'attachment;filename=' + str(id_) + '.json'})
 
 
-@app.route('/api/0.1')
+@app.route('/api/0.1', methods=['GET'])
 @api_access()  # type: ignore
 def api_get_multiple_entities() -> Response:  # pragma: no cover
     entity = request.args.getlist('entity')
     return jsonify(Api.get_entities_by_id(ids=entity))
 
 
-@app.route('/api/0.1/code/<code>')
+@app.route('/api/0.1/code/<code>', methods=['GET'])
 @api_access()  # type: ignore
 def api_get_by_code(code: str) -> Response:
     return jsonify(Api.get_entities_by_code(code_=code))
 
 
-@app.route('/api/0.1/class/<class_code>')
+@app.route('/api/0.1/class/<class_code>', methods=['GET'])
 @api_access()  # type: ignore
 def api_get_by_class(class_code: str) -> Response:
     return jsonify(Api.get_entities_by_class(class_code_=class_code))
 
 
-@app.route('/api/0.1/latest/<int:limit>')
+@app.route('/api/0.1/latest/<int:limit>', methods=['GET'])
 @api_access()  # type: ignore
 def api_get_latest(limit: int) -> Response:
     return jsonify(Api.get_entities_get_latest(limit_=limit))
