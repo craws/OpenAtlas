8a7f716a6538f4e1f80470219ad0eaa6b7d04092
Alexander Watzinger <alexander.watzinger@craws.net>
2020-06-20 17:32:29 +0200

    Tabs template

--- openatlas/util/tab.py
+++ openatlas/util/tab.py
@@ -4,7 +4,7 @@ from flask import url_for
 
 from openatlas.models.entity import Entity
 from openatlas.util.table import Table
-from openatlas.util.util import button, uc_first
+from openatlas.util.util import button, is_authorized, uc_first
 from flask_babel import lazy_gettext as _
 
 # needed for translations
@@ -12,6 +12,8 @@ _('member of')
 
 
 class Tab:
+    id: str
+    origin: Optional[Entity]
 
     def __init__(self,
                  id_: str,
@@ -22,14 +24,22 @@ class Tab:
         self.title = uc_first(_(id_.replace('_', ' ')))
         self.buttons = buttons if buttons else []
         self.table = table
-        if origin and id_ == 'source':
-            if origin.class_.code == 'E31':
-                self.buttons = [
-                    button(_('add'), url_for('reference_add', id_=origin.id, class_name='source'))]
-            if origin.system_type == 'file':
-                self.buttons = [
-                    button(_('add'), url_for('file_add', id_=origin.id, class_name='source'))]
-            else:
-                self.buttons = [button(_('add'), url_for('entity_add_source', id_=origin.id))]
-            self.buttons.append(button(_('source'), url_for('source_insert', origin_id=origin.id)))
+        self.origin = origin
+        if self.origin and is_authorized('contributor'):
+            self.buttons = self.add_buttons()
 
+    def add_buttons(self):
+        buttons = []
+        if self.id == 'source':
+            if self.origin.class_.code == 'E31':
+                buttons.append(button(_('add'), url_for('reference_add',
+                                                        id_=self.origin.id,
+                                                        class_name='source')))
+            if self.origin.system_type == 'file':
+                buttons.append(button(_('add'), url_for('file_add',
+                                                        id_=self.origin.id,
+                                                        class_name='source')))
+            else:
+                buttons.append(button(_('add'), url_for('entity_add_source', id_=self.origin.id)))
+            buttons.append(button(_('source'), url_for('source_insert', origin_id=self.origin.id)))
+        return buttons
