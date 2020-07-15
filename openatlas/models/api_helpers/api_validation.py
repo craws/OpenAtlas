6f67aea148d16319f691c5db17e982b5d273248c
BernhardKoschicek <b.koschicek@gmail.com>
2020-07-15 16:56:52 +0200

    API: Filtering (validation and basic implementation)

--- openatlas/models/api_helpers/api_validation.py
+++ openatlas/models/api_helpers/api_validation.py
@@ -8,6 +8,9 @@ class Validation:
               'begin_from', 'begin_to', 'end_from', 'end_to']
     operators = ['eq', 'ne', 'lt', 'le', 'gt', 'ge', 'and', 'or', 'not', 'contains', 'startsWith',
                  'in', 'match']
+    operators_dict = {'eq': '=', 'ne': '!=', 'lt': '<', 'le': '<=', 'gt': '>', 'ge': '>=',
+                      'and': 'AND', 'or': 'OR', 'onot': 'OR NOT', 'anot': 'AND NOT',
+                      'contains': 'CONTAINS', 'like': 'LIKE', 'in': 'IN'}
 
     @staticmethod
     def validate_url_query(query):
@@ -15,22 +18,29 @@ class Validation:
                  'limit': Validation.validate_limit(query.getlist('limit')),
                  'sort': Validation.validate_sort(query.getlist('sort')),
                  'column': Validation.validate_column(query.getlist('column'))}
-
         return query
 
     @staticmethod
     def validate_filter(filter):
-        filter = re.findall(r'\(.*?\)', ''.join(filter))
-        print(type(filter))
+        filter = re.findall(r'(\w+)\((.*?)\)', ''.join(filter))
+        filter_query = ''
         for item in filter:
-            item = tuple(item)
-            for i in item:
-                print(i)
-            if item[0] in Validation.operators and item[1] in Validation.column:
-                print(item)
+            operator = item[0].lower()
+            if operator in Validation.operators_dict:
+                filter_query += Validation.operators_dict[operator]
+                item = re.split('[,:;]', item[1])
+                if item[0] in Validation.operators_dict and item[1] in Validation.column:
+                    if item[0] == 'like':
+                        item[2] = '\'' + item[2] + '\''
+
+                    filter_query += ' ' + item[1] + ' ' \
+                                    + Validation.operators_dict[item[0]] + ' ' + item[2] + ' '
+                else:
+                    filter_query = Validation.default['filter']
             else:
-                filter = Validation.default['filter']
-        return Validation.default['filter']
+                filter_query = Validation.default['filter']
+        print(filter_query)
+        return filter_query
 
     @staticmethod
     def validate_limit(limit):
