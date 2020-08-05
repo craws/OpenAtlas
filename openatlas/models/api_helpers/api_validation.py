4720908371c0cd5a89abb42f6027d493696c75a3
BernhardKoschicek <b.koschicek@gmail.com>
2020-08-05 13:02:36 +0200

    Mypy checks and clean up

--- openatlas/models/api_helpers/api_validation.py
+++ openatlas/models/api_helpers/api_validation.py
@@ -109,8 +109,8 @@ class Validation:
         return first_[0]
 
     @staticmethod
-    def validate_show(show: List[str]) -> Union[List[str], str, None]:
-        show_: Union[List[str], str, None] = []
+    def validate_show(show: List[str]) -> List[str]:
+        show_ = []
         valid = ['when', 'types', 'relations', 'names', 'links', 'geometry', 'depictions', 'not']
         for pattern in valid:
             if re.search(pattern, str(show)):
