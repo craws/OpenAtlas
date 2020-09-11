79d5744a796b556f92742a1fdc3912a6abec1f29
Alexander Watzinger <alexander.watzinger@craws.net>
2020-09-11 17:46:17 +0200

    Update sql upgrade script

--- install/upgrade/5.5.0.sql
+++ install/upgrade/5.5.0.sql
@@ -1,7 +1,10 @@
--- Upgrade 5.3.0 to 5.4.0
+-- Upgrade 5.4.0 to 5.5.0
 -- Be sure to backup the database and read the upgrade notes before executing this!
 
 BEGIN;
+
+-- Remove leading and trailing white spaces from names
+UPDATE model.entity SET name = TRIM(name);
 
 -- #929: Module options
 INSERT INTO web.settings (name, value) VALUES
