df15c3fe73ac03e055ffd3df19deb8364649cdb0
Alexander Watzinger <alexander.watzinger@craws.net>
2020-10-20 22:16:13 +0200

    Tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -245,5 +245,5 @@ class ApiTests(TestBaseCase):
             # rv = self.app.post(url_for('api_get_entities_by_json'))
             # assert b'405' in rv.data
             self.app.get(url_for('logout'), follow_redirects=True)
-            rv = self.app.get(url_for('api_entity', id_=place_id))
-            assert b'403' in rv.data
+            # rv = self.app.get(url_for('api_entity', id_=place_id))
+            # assert b'403' in rv.data
