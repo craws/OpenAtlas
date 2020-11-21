7a06d50f7bbf3d0e7b14317837a253c0637e294f
BernhardKoschicek <b.koschicek@gmail.com>
2020-11-21 21:47:50 +0100

    made reference for parameter

--- openatlas/api/v02/routes.py
+++ openatlas/api/v02/routes.py
@@ -15,9 +15,10 @@ from openatlas.api.v02.common.subunit_hierarchy import GetSubunitHierarchy
 
 template = {
     "swagger": "2.0",
+    # "openapi": "3.0.0",
     "info": {
         "title": "Openatlas API",
-        "verion": "0.2",
+        "version": "0.2",
         "description": "A documentation of the OpenAtlas API",
         "license": {
             "name": "Apache 2.0",
@@ -26,6 +27,129 @@ template = {
         "contact": {
             "name": "Bernhard Koschicek",
             "email": "bernhard.koschicek@oeaw.ac.at"
+        }
+    },
+    "tags": [
+        {
+            "name": "Entities",
+            "description": "Requesting entities through different means."
+        },
+        {
+            "name": "Nodes",
+            "description": "Requesting nodes and subunits"
+        },
+        {
+            "name": "Content",
+            "description": "Requesting content of the OpenAtlas instance."
+        }
+    ],
+    "components": {
+        "parameters": {
+            "limitParam": {
+                "name": "limit",
+                "in": "query",
+                "description": "Number of geojson representation to be returned",
+                "schema": {
+                    "type": "number"
+                }
+            },
+            "columnParam": {
+                "name": "column",
+                "in": "query",
+                "description": "The result will sorted by the given column",
+                "schema": {
+                    "type": "string",
+                    "enum": [
+                        "id",
+                        "class_code",
+                        "name",
+                        "description",
+                        "created",
+                        "modified",
+                        "system_type",
+                        "begin_from",
+                        "begin_to",
+                        "end_from",
+                        "end_to"
+                    ]
+                }
+            },
+            "sortParam": {
+                "name": "sort",
+                "in": "query",
+                "description": "Result will be sorted asc/desc (by default by the name column)",
+                "schema": {
+                    "type": "string",
+                    "enum": ["asc", "desc"]
+                }
+            },
+            "filterParam": {
+                "name": "filter",
+                "in": "query",
+                "description": "Specify request with custom SQL filter method",
+                "schema": {
+                    "type": "string"
+                }
+            },
+            "firstParam": {
+                "name": "first",
+                "in": "query",
+                "description": "List of results start with given ID",
+                "schema": {
+                    "type": "number"
+                }
+            },
+            "lastParam": {
+                "name": "last",
+                "in": "query",
+                "description": "List of results start with entity after given ID",
+                "schema": {
+                    "type": "number"
+                }
+            },
+            "countParam": {
+                "name": "count",
+                "in": "query",
+                "description": "Returns a number which represents the total count of the result",
+                "schema": {
+                    "type": "boolean"
+                }
+            },
+            "downloadParam": {
+                "name": "download",
+                "in": "query",
+                "description": "Triggers the file download of the given request",
+                "schema": {
+                    "type": "boolean"
+                }
+            },
+            "showParam": {
+                "name": "show",
+                "in": "query",
+                "description": "Select which key should be shown e.g. when, types, relations, names, links, geometry, depictions, not",
+                "schema": {
+                    "type": "string",
+                    "enum": [
+                         "when",
+                         "types",
+                         "relations",
+                         "names",
+                         "links",
+                         "geometry",
+                         "depictions",
+                         "not"
+                    ]
+                }
+            },
+            "langParam": {
+                "name": "language",
+                "in": "query",
+                "description": "Select output language",
+                "schema": {
+                    "type": "string",
+                    "enum": ["en", "de"]
+                }
+            },
         }
     }
 }
