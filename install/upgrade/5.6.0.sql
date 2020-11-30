b8d7f574d029ca4dff2962024de25b0439f8c34b
Alexander Watzinger <alexander.watzinger@craws.net>
2020-11-30 17:21:41 +0100

    Update install and upgrade SQL

--- install/upgrade/5.6.0.sql
+++ install/upgrade/5.6.0.sql
@@ -8,6 +8,8 @@ INSERT INTO web.settings (name, value) VALUES ('module_wikidata', 'True');
 
 -- #1348 Refactor forms - fix capitalization for automatic standard type detection
 UPDATE web.form SET name = 'Source Translation' WHERE name = 'Source translation';
+UPDATE model.entity SET name = 'Source Translation' WHERE name = 'Source translation';
+UPDATE web.hierarchy SET name = 'Source Translation' WHERE name = 'Source translation';
 
 -- #1393 Split profile option - removing obsolete layout value
 DELETE FROM web.user_settings WHERE name = 'layout';
