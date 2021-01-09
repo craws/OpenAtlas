7d1070a3416c7ba535c1d67ef8a57d5380aa32de
Alexander Watzinger <alexander.watzinger@craws.net>
2021-01-09 23:54:34 +0100

    Reference systems: moved validation and JavaScript functionallity to new fields

--- openatlas/util/filters.py
+++ openatlas/util/filters.py
@@ -409,7 +409,7 @@ def display_form(self: Any,
         # External reference system
         if field.id.startswith('reference_system_id_'):
             precision_field = getattr(form, field.id.replace('id_', 'precision_'))
-            html += add_row(field, field.label, ' '.join([str(field),
+            html += add_row(field, field.label, ' '.join([str(field(class_=field.label.text)),
                                                           str(precision_field.label),
                                                           str(precision_field)]))
             continue
