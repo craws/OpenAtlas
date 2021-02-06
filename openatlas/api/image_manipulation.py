ef8a0819e564a9cee41e2f6d5c8782947a7a85ed
BernhardKoschicek <b.koschicek@gmail.com>
2021-02-06 19:11:32 +0100

    playing around

--- openatlas/api/image_manipulation.py
+++ openatlas/api/image_manipulation.py
@@ -1,6 +1,9 @@
 from __future__ import print_function
 
+import tempfile
 from io import BytesIO
+from tempfile import NamedTemporaryFile
+
 from wand.image import Image
 from wand.display import display
 
@@ -9,14 +12,17 @@ class ImageManipulation:
 
     @staticmethod
     def image_thumbnail(path: str, thumb_size: int) -> BytesIO:
-        with Image(filename=path) as img:
-            with img.clone() as i:
-                img_io = BytesIO()
-                i.sample(int(thumb_size), int(thumb_size))
-                i.rotate(180)
-                i.save(file=img_io)
-                #i.save(filename=path.format('1'))
-        return i
+        with Image(filename=path) as src:
+            with src.clone() as img:
+
+                w, h = int(thumb_size), int(thumb_size),
+                img.resize(w, h)
+                tf = tempfile.NamedTemporaryFile()
+                img.save(file=tf)
+
+        return img
+
+
         # img_io = BytesIO()
         # img = Image.open(path)
         # img.thumbnail((thumb_size, thumb_size))
