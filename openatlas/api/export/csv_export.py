a51f262c2e80f03ae438a0198cb88b746612afe6
BernhardKoschicek <b.koschicek@gmail.com>
2021-03-26 11:30:40 +0100

    addes export to swagger.json

--- openatlas/api/export/csv_export.py
+++ openatlas/api/export/csv_export.py
@@ -1,4 +1,5 @@
-from collections import defaultdict
+from collections import Counter, defaultdict
+from typing import List
 
 import pandas as pd
 
@@ -11,24 +12,22 @@ class ApiExportCSV:
     @staticmethod
     def export_entity(entity: Entity):
         data = {
-            'id': str(entity.id),
-            'name': entity.name,
-            'description': entity.description,
-            'begin_from': entity.begin_from,
-            'begin_to': entity.begin_to,
-            'begin_comment': entity.begin_comment,
-            'end_from': entity.end_from,
-            'end_to': entity.end_to,
-            'end_comment': entity.end_comment,
-            'cidoc_class': entity.cidoc_class.name,
-            'system_class': entity.class_.name,
-            'note': entity.note}
+            'id': [str(entity.id)],
+            'name': [entity.name],
+            'description': [entity.description],
+            'begin_from': [entity.begin_from],
+            'begin_to': [entity.begin_to],
+            'begin_comment': [entity.begin_comment],
+            'end_from': [entity.end_from],
+            'end_to': [entity.end_to],
+            'end_comment': [entity.end_comment],
+            'cidoc_class': [entity.cidoc_class.name],
+            'system_class': [entity.class_.name],
+            'note': [entity.note]}
 
         for k, v in ApiExportCSV.get_links(entity).items():
-            data[k] = ' | '.join(list(map(str, v)))
-        print(data)
+            data[k] = [' | '.join(list(map(str, v)))]
         df = pd.DataFrame.from_dict(data=data, orient='index').T
-        print(df)
         df.to_csv('test.csv')
         return data
 
