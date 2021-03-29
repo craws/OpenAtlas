2101a0bb3945c5b8c7526c40036ee9a31ef373b9
Alexander Watzinger <alexander.watzinger@craws.net>
2021-03-29 13:39:24 +0200

    Database interface: reference system

---
+++ openatlas/database/reference_system.py
@@ -0,0 +1,84 @@
+from typing import Any, Dict, List, Union
+
+from flask import g
+
+
+class ReferenceSystem:
+
+    @staticmethod
+    def get_all() -> List[Dict[str, Any]]:
+        g.cursor.execute("""
+            SELECT
+                e.id, e.name, e.class_code, e.description, e.system_class, e.created, e.modified,
+                rs.website_url, rs.resolver_url, rs.identifier_example, rs.system,
+                rs.precision_default_id, COUNT(l.id) AS count,
+                (SELECT ARRAY(
+                    SELECT f.id FROM web.form f
+                    JOIN web.reference_system_form rfs ON f.id = rfs.form_id
+                        AND rfs.reference_system_id = rs.entity_id)) AS form_ids,
+                array_to_json(
+                    array_agg((t.range_id, t.description)) FILTER (WHERE t.range_id IS NOT NULL)
+                ) AS nodes
+            FROM model.entity e
+            JOIN web.reference_system rs ON e.id = rs.entity_id
+            LEFT JOIN model.link l ON e.id = l.domain_id AND l.property_code = 'P67'
+            LEFT JOIN model.link t ON e.id = t.domain_id AND t.property_code = 'P2'
+            GROUP BY
+                e.id, e.name, e.class_code, e.description, e.system_class, e.created,
+                e.modified, rs.website_url, rs.resolver_url, rs.identifier_example, rs.system,
+                rs.precision_default_id, rs.entity_id;""")
+        return [dict(row) for row in g.cursor.fetchall()]
+
+    @staticmethod
+    def add_forms(entity_id: int, form_ids: List[int]) -> None:
+        for form_id in form_ids:
+            sql = """
+                INSERT INTO web.reference_system_form (reference_system_id, form_id)
+                VALUES (%(entity_id)s, %(form_id)s);"""
+            g.cursor.execute(sql, {'entity_id': entity_id, 'form_id': form_id})
+
+    @staticmethod
+    def remove_form(entity_id: int, form_id: int) -> None:
+        sql = """
+            DELETE FROM web.reference_system_form
+            WHERE reference_system_id = %(reference_system_id)s AND form_id = %(form_id)s;"""
+        g.cursor.execute(sql, {'reference_system_id': entity_id, 'form_id': form_id})
+
+    @staticmethod
+    def get_forms(form_id: int) -> List[Dict[str, Union[int, str]]]:
+        sql = """
+            SELECT f.id, f.name FROM web.form f
+            JOIN web.reference_system_form rsf ON f.id = rsf.form_id
+                AND rsf.reference_system_id = %(id)s;"""
+        g.cursor.execute(sql, {'id': form_id})
+        return [dict(row) for row in g.cursor.fetchall()]
+
+    @staticmethod
+    def update_system(data: Dict[str, Any]) -> None:
+        sql = '''
+            UPDATE web.reference_system SET (name, website_url, resolver_url, identifier_example,
+                precision_default_id)
+            = (%(name)s, %(website_url)s, %(resolver_url)s, %(identifier_example)s,
+                %(precision_default_id)s)
+            WHERE entity_id = %(entity_id)s;'''
+        g.cursor.execute(sql, data)
+
+    @staticmethod
+    def remove_link(system_id: int, entity_id: int) -> None:
+        sql = """
+            DELETE FROM model.link WHERE property_code = 'P67'
+            AND domain_id = %(system_id)s AND range_id = %(entity_id)s;"""
+        g.cursor.execute(sql, {'system_id': system_id, 'entity_id': entity_id})
+
+    @staticmethod
+    def get_form_choices(form_names: List[str]) -> List[Dict[str, Union[str, int]]]:
+        sql = "SELECT f.id, f.name FROM web.form f WHERE f.name IN %(forms)s ORDER BY name ASC;"
+        g.cursor.execute(sql, {'forms': tuple(form_names)})
+        return [dict(row) for row in g.cursor.fetchall()]
+
+    @staticmethod
+    def insert_system(data: Dict[str, Any]) -> None:
+        sql = '''
+            INSERT INTO web.reference_system (entity_id, name, website_url, resolver_url)
+            VALUES (%(entity_id)s, %(name)s, %(website_url)s, %(resolver_url)s);'''
+        g.cursor.execute(sql, data)
--- openatlas/models/reference_system.py
+++ openatlas/models/reference_system.py
@@ -1,12 +1,12 @@
 from __future__ import annotations  # Needed for Python 4.0 type annotations
 
-from typing import Dict, List, Tuple, Union
+from typing import Any, Dict, List, Tuple, Union
 
 from flask import g
 from flask_wtf import FlaskForm
-from psycopg2.extras import NamedTupleCursor
 
 from openatlas import app
+from openatlas.database.reference_system import ReferenceSystem as Db
 from openatlas.models.entity import Entity
 
 
@@ -16,41 +16,20 @@ class ReferenceSystem(Entity):
     placeholder = None
     system = False
 
-    def __init__(self, row: NamedTupleCursor.Record) -> None:
+    def __init__(self, row: Dict[str, Any]) -> None:
 
         super().__init__(row)
-        self.website_url = row.website_url
-        self.resolver_url = row.resolver_url
-        self.forms = row.form_ids
-        self.placeholder = row.identifier_example
-        self.precision_default_id = row.precision_default_id
-        self.count = row.count
-        self.system = row.system
+        self.website_url = row['website_url']
+        self.resolver_url = row['resolver_url']
+        self.forms = row['form_ids']
+        self.placeholder = row['identifier_example']
+        self.precision_default_id = row['precision_default_id']
+        self.count = row['count']
+        self.system = row['system']
 
     @staticmethod
     def get_all() -> Dict[int, ReferenceSystem]:
-        sql = """
-            SELECT
-                e.id, e.name, e.class_code, e.description, e.system_class, e.created, e.modified,
-                rs.website_url, rs.resolver_url, rs.identifier_example, rs.system,
-                rs.precision_default_id, COUNT(l.id) AS count,
-                (SELECT ARRAY(
-                    SELECT f.id FROM web.form f
-                    JOIN web.reference_system_form rfs ON f.id = rfs.form_id
-                        AND rfs.reference_system_id = rs.entity_id)) AS form_ids,
-                array_to_json(
-                    array_agg((t.range_id, t.description)) FILTER (WHERE t.range_id IS NOT NULL)
-                ) AS nodes
-            FROM model.entity e
-            JOIN web.reference_system rs ON e.id = rs.entity_id
-            LEFT JOIN model.link l ON e.id = l.domain_id AND l.property_code = 'P67'
-            LEFT JOIN model.link t ON e.id = t.domain_id AND t.property_code = 'P2'
-            GROUP BY
-                e.id, e.name, e.class_code, e.description, e.system_class, e.created,
-                e.modified, rs.website_url, rs.resolver_url, rs.identifier_example, rs.system,
-                rs.precision_default_id, rs.entity_id;"""
-        g.cursor.execute(sql)
-        return {row.id: ReferenceSystem(row) for row in g.cursor.fetchall()}
+        return {row['id']: ReferenceSystem(row) for row in Db.get_all()}
 
     @staticmethod
     def get_by_name(name: str) -> ReferenceSystem:
@@ -59,29 +38,17 @@ class ReferenceSystem(Entity):
                 return system
 
     def add_forms(self, form: FlaskForm) -> None:
-        for form_id in form.forms.data:
-            sql = """
-                INSERT INTO web.reference_system_form (reference_system_id, form_id)
-                VALUES (%(entity_id)s, %(form_id)s);"""
-            g.cursor.execute(sql, {'entity_id': self.id, 'form_id': form_id})
+        Db.add_forms(self.id, form.forms.data)
 
     def remove_form(self, form_id: int) -> None:
         forms = self.get_forms()
         for link_ in self.get_links('P67'):
             if link_.range.class_.name == forms[form_id]['name']:  # pragma: no cover
-                return  # Don't continue if entities are still attached
-        sql = """
-            DELETE FROM web.reference_system_form
-            WHERE reference_system_id = %(reference_system_id)s AND form_id = %(form_id)s;"""
-        g.cursor.execute(sql, {'reference_system_id': self.id, 'form_id': form_id})
+                return  # Abort if there are linked entities
+        Db.remove_form(self.id, form_id)
 
     def get_forms(self) -> Dict[int, Dict[str, str]]:
-        sql = """
-            SELECT f.id, f.name FROM web.form f
-            JOIN web.reference_system_form rsf ON f.id = rsf.form_id
-                AND rsf.reference_system_id = %(id)s;"""
-        g.cursor.execute(sql, {'id': self.id})
-        return {row.id: {'name': row.name} for row in g.cursor.fetchall()}
+        return {row['id']: {'name': row['name']} for row in Db.get_forms(self.id)}
 
     def update_system(self, form: FlaskForm) -> None:
         self.update(form)
@@ -89,13 +56,7 @@ class ReferenceSystem(Entity):
         entity_with_updated_nodes = Entity.get_by_id(self.id, nodes=True)
         if entity_with_updated_nodes.nodes:  # Get default precision id if it was set
             precision_default_id = list(entity_with_updated_nodes.nodes.keys())[0].id
-        sql = '''
-            UPDATE web.reference_system SET (name, website_url, resolver_url, identifier_example,
-                precision_default_id)
-            = (%(name)s, %(website_url)s, %(resolver_url)s, %(identifier_example)s,
-                %(precision_default_id)s)
-            WHERE entity_id = %(entity_id)s;'''
-        g.cursor.execute(sql, {
+        Db.update_system({
             'entity_id': self.id,
             'name': self.name,
             'website_url': self.website_url,
@@ -109,33 +70,24 @@ class ReferenceSystem(Entity):
             if field.id.startswith('reference_system_id_'):  # Delete and recreate link
                 system = Entity.get_by_id(int(field.id.replace('reference_system_id_', '')))
                 precision_field = getattr(form, field.id.replace('id_', 'precision_'))
-                sql = """
-                    DELETE FROM model.link WHERE property_code = 'P67'
-                    AND domain_id = %(system_id)s AND range_id = %(entity_id)s;"""
-                g.cursor.execute(sql, {'system_id': system.id, 'entity_id': entity.id})
+                Db.remove_link(system.id, entity.id)
                 if field.data:
                     system.link('P67', entity, field.data, type_id=precision_field.data)
 
     @staticmethod
     def get_form_choices(entity: Union[ReferenceSystem, None]) -> List[Tuple[int, str]]:
-        g.cursor.execute(
-            "SELECT f.id, f.name FROM web.form f WHERE f.name IN %(forms)s ORDER BY name ASC",
-            {'forms': tuple(app.config['EXTERNAL_REFERENCES_FORMS'])})
         choices = []
-        for row in g.cursor.fetchall():
-            if not entity or row.id not in entity.forms:
-                if entity and entity.name == 'GeoNames' and row.name != 'Place':
+        for row in Db.get_form_choices(app.config['EXTERNAL_REFERENCES_FORMS']):
+            if not entity or row['id'] not in entity.forms:
+                if entity and entity.name == 'GeoNames' and row['name'] != 'Place':
                     continue
-                choices.append((row.id, row.name))
+                choices.append((row['id'], row['name']))
         return choices
 
     @staticmethod
     def insert_system(form: FlaskForm) -> Entity:
         entity = Entity.insert('reference_system', form.name.data, form.description.data)
-        sql = '''
-            INSERT INTO web.reference_system (entity_id, name, website_url, resolver_url)
-            VALUES (%(entity_id)s, %(name)s, %(website_url)s, %(resolver_url)s);'''
-        g.cursor.execute(sql, {
+        Db.insert_system({
             'entity_id': entity.id,
             'name': entity.name,
             'website_url': form.website_url.data if form.website_url.data else None,
