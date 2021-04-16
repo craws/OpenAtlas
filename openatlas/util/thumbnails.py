24a391f7c35d44fe2f8ea0f4f191545e9f700a6e
BernhardKoschicek <b.koschicek@gmail.com>
2021-04-16 13:39:42 +0200

    refactored and cleaned

--- openatlas/util/thumbnails.py
+++ openatlas/util/thumbnails.py
@@ -3,30 +3,29 @@ from wand.image import Image
 from openatlas import app
 
 
-class ImageManipulation:
-
+class Thumbnails:
     multi_image = ['pdf', 'mp4', 'gif', 'psd', 'ai', 'xcf']
     single_image = ['jpeg', 'jpg', 'png', 'tiff', 'tif', 'raw', 'eps']
 
     @staticmethod
-    def upload_image(filename: str) -> None:
+    def upload_to_thumbnail(filename: str) -> None:
         name = filename.rsplit('.', 1)[0].lower()
         file_format = filename.rsplit('.', 1)[1].lower()
-        if file_format in ImageManipulation.single_image + ImageManipulation.multi_image:
-            ImageManipulation.safe_as_thumbnail(name, file_format)
+        if file_format in Thumbnails.single_image + Thumbnails.multi_image:
+            Thumbnails.safe_as_thumbnail(name, file_format)
 
     @staticmethod
     def safe_as_thumbnail(filename: str, file_format: str) -> None:
         path = str(app.config['UPLOAD_DIR']) + '/' + filename + '.' + file_format
-        if file_format in ImageManipulation.multi_image:
+        if file_format in Thumbnails.multi_image:
             path += '[0]'
         with Image(filename=path) as src:
             with src.convert('png') as img:
                 # https://docs.wand-py.org/en/0.6.6/guide/resizecrop.html?highlight=down%20scale#transform-images
                 img.transform(resize='400x400>')
-                #img.compression_quality = 75
-                img.save(filename=str(app.config['UPLOAD_DIR']) + '/thumbnails/' + filename + '.png')
-
+                # img.compression_quality = 75
+                img.save(
+                    filename=str(app.config['UPLOAD_DIR']) + '/thumbnails/' + filename + '.png')
 
     # @staticmethod
     # def image_resize(filename: str, thumb_size: int) -> Image:
