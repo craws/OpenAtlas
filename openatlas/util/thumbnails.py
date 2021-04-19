5952d85657588ee519f980f29f8201f901657b6d
BernhardKoschicek <b.koschicek@gmail.com>
2021-04-19 13:08:02 +0200

    variable thumb size

--- openatlas/util/thumbnails.py
+++ openatlas/util/thumbnails.py
@@ -12,31 +12,20 @@ class Thumbnails:
         name = filename.rsplit('.', 1)[0].lower()
         file_format = filename.rsplit('.', 1)[1].lower()
         if file_format in Thumbnails.single_image + Thumbnails.multi_image:
-            Thumbnails.safe_as_thumbnail(name, file_format)
+            sizes = ['400x400>']
+            for size in sizes:
+                Thumbnails.safe_as_thumbnail(name, file_format, size)
 
     @staticmethod
-    def safe_as_thumbnail(filename: str, file_format: str) -> None:
+    def safe_as_thumbnail(filename: str, file_format: str, size: str) -> None:
         path = str(app.config['UPLOAD_DIR']) + '/' + filename + '.' + file_format
         if file_format in Thumbnails.multi_image:
             path += '[0]'
         with Image(filename=path) as src:
             with src.convert('png') as img:
                 # https://docs.wand-py.org/en/0.6.6/guide/resizecrop.html?highlight=down%20scale#transform-images
-                img.transform(resize='400x400>')
+                img.transform(resize=size)
                 # img.compression_quality = 75
                 img.save(
                     filename=str(app.config['UPLOAD_DIR']) + '/thumbnails/' + filename + '.png')
 
-    # @staticmethod
-    # def image_resize(filename: str, thumb_size: int) -> Image:
-    #     path = str(app.config['UPLOAD_DIR']) + '/' + filename
-    #     with Image(filename=path) as src:
-    #         print(src.size)
-    #         print(path)
-    #         with src.clone() as img:
-    #             size = str(thumb_size)
-    #             # https://docs.wand-py.org/en/0.6.6/guide/resizecrop.html?highlight=down%20scale#transform-images
-    #             img.transform(resize=size+'x'+size+'>')
-    #             print(type(img))
-    #             return img
-    #
