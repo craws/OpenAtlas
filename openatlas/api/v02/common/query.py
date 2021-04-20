aa27a27435bc625a18cd0e111cdc28b1a04a0d06
BernhardKoschicek <b.koschicek@gmail.com>
2021-04-20 10:20:12 +0200

    refactor

--- openatlas/api/v02/common/query.py
+++ openatlas/api/v02/common/query.py
@@ -9,10 +9,10 @@ from openatlas.api.v02.common.code import GetByCode
 from openatlas.api.v02.common.system_class import GetBySystemClass
 from openatlas.api.v02.resources.download import Download
 from openatlas.api.v02.resources.error import QueryEmptyError
-from openatlas.api.v02.resources.geojson_entity import GeoJsonEntity
+from openatlas.api.v02.resources.linked_places import LinkedPlacesEntity
 from openatlas.api.v02.resources.pagination import Pagination
 from openatlas.api.v02.resources.parser import query_parser
-from openatlas.api.v02.templates.geojson import GeoJson
+from openatlas.api.v02.templates.linked_places import LinkedPlacesTemplate
 from openatlas.util.util import api_access
 
 
@@ -29,7 +29,7 @@ class GetQuery(Resource):  # type: ignore
         entities = []
         if parser['entities']:
             for entity in parser['entities']:
-                entities.append(GeoJsonEntity.get_entity_by_id(entity))
+                entities.append(LinkedPlacesEntity.get_entity_by_id(entity))
         if parser['codes']:
             for code_ in parser['codes']:
                 entities.extend(GetByCode.get_entities_by_view(code_=code_, parser=parser))
@@ -44,7 +44,7 @@ class GetQuery(Resource):  # type: ignore
         output = Pagination.pagination(entities=entities, parser=parser)
         if parser['count']:
             return jsonify(output['pagination']['entities'])
-        template = GeoJson.pagination(parser['show'])
+        template = LinkedPlacesTemplate.pagination(parser['show'])
         if parser['download']:
             return Download.download(data=output, template=template, name='query')
         return marshal(output, template), 200
--- openatlas/api/v02/common/type_entities_all.py
+++ openatlas/api/v02/common/type_entities_all.py
@@ -6,10 +6,10 @@ from flask_restful import Resource, marshal
 
 from openatlas.api.v02.resources.download import Download
 from openatlas.api.v02.resources.error import InvalidSubunitError
-from openatlas.api.v02.resources.geojson_entity import GeoJsonEntity
+from openatlas.api.v02.resources.linked_places import LinkedPlacesEntity
 from openatlas.api.v02.resources.pagination import Pagination
 from openatlas.api.v02.resources.parser import entity_parser
-from openatlas.api.v02.templates.geojson import GeoJson
+from openatlas.api.v02.templates.linked_places import LinkedPlacesTemplate
 from openatlas.util.util import api_access
 
 
@@ -20,11 +20,11 @@ class GetTypeEntitiesAll(Resource):  # type: ignore
         parser = entity_parser.parse_args()
         entities = []
         for entity in GetTypeEntitiesAll.get_node_all(id_):
-            entities.append(GeoJsonEntity.get_entity_by_id(entity))
+            entities.append(LinkedPlacesEntity.get_entity_by_id(entity))
         if parser['count']:
             return jsonify(len(entities))
         output = Pagination.pagination(entities=entities, parser=parser)
-        template = GeoJson.pagination(parser['show'])
+        template = LinkedPlacesTemplate.pagination(parser['show'])
         if parser['download']:
             return Download.download(data=output, template=template, name=id_)
         return marshal(output, template), 200
