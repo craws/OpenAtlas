76b427a6496d45c2caf85014542982a0354a18f8
Alexander Watzinger <alexander.watzinger@craws.net>
2021-04-27 14:48:12 +0200

    CONNEC letter restructure script

--- openatlas/views/connec.py
+++ openatlas/views/connec.py
@@ -9,8 +9,6 @@ from openatlas import app
 # Make sure to set database in instance/production.py to openatlas_connec
 
 # To do
-# move letter types from source super to artifact super
-# create new letter type for source, link sources
 # add case study to artifact form
 # copy case study types from source to artifacts
 # create source - artifact links
@@ -21,25 +19,31 @@ from openatlas import app
 # Check
 # got all letters? which types are still needed (ask CONNEC, check move event sources)
 # relevant custom types which should move from source to artifact?
+from openatlas.models.entity import Entity
+from openatlas.models.link import Link
+
+source_type_id = 35
+artifact_type_id = 9417
+letter_type_root_ids = [38, 1255]
 
 
 @app.route('/connec')
 def restructure_connec():
     output = ['Setup database']
     setup()
-    letter_type_ids = [38, 1255] + g.nodes[38].subs + g.nodes[1255].subs
+    letter_type_ids = letter_type_root_ids + g.nodes[38].subs + g.nodes[1255].subs
     output.append(f'Letter tye ids: {letter_type_ids}')
-    letters = []
+    letter_sources = []
     for id_ in letter_type_ids:
-        letters += g.nodes[id_].get_linked_entities('P2', inverse=True, nodes=True)
-    output.append(f'Sources with letter type: {len(letters)}')
-    source_artifact_ids = insert_artifacts(letters)
-    update_types(source_artifact_ids, letter_type_ids)
+        letter_sources += g.nodes[id_].get_linked_entities('P2', inverse=True, nodes=True)
+    output.append(f'Sources with letter type: {len(letter_sources)}')
+    source_artifact_ids = insert_artifacts(letter_sources)
+    update_types(source_artifact_ids, letter_type_ids, letter_sources)
     output.append('Done')
     return '<br>'.join(output)
 
 
-def update_types(source_artifact_ids, letter_type_ids):
+def update_types(source_artifact_ids, letter_type_ids, letter_sources):
     sql = ''
     for source_id, artifact_id in source_artifact_ids.items():
         sql += f"""
@@ -48,7 +52,26 @@ def update_types(source_artifact_ids, letter_type_ids):
                 domain_id = {source_id} 
                 AND property_code = 'P2'
                 AND range_id in {tuple(letter_type_ids)};"""
-    print(sql)
+    # Move letter root types from source super to artifact super
+    for id_ in letter_type_root_ids:
+        sql += f"""
+            UPDATE model.link SET range_id = {artifact_type_id}
+            WHERE property_code = 'P127' AND domain_id = {id_} AND range_id = {source_type_id};"""
+    g.connec_cursor.execute(sql)
+
+    # Create source type "letter", connect entries
+    sql = """
+        INSERT INTO model.entity (name, system_class, class_code)
+        VALUES ('Letter', 'type', 'E55') RETURNING id;"""
+    g.connec_cursor.execute(sql)
+    letter_type_id = g.connec_cursor.fetchone()['id']
+    sql = f"""
+        INSERT INTO model.link (property_code, domain_id, range_id)
+        VALUES ('P127', {letter_type_id}, {source_type_id});"""
+    for letter in letter_sources:
+        sql += f"""
+            INSERT INTO model.link (property_code, domain_id, range_id)
+            VALUES ('P2', {letter.id}, {letter_type_id});"""
     g.connec_cursor.execute(sql)
 
 
