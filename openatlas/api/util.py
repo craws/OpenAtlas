ab2e51f22b342d5957221a7088d1e01360406dc5
BernhardKoschicek <b.koschicek@gmail.com>
2021-06-07 15:14:50 +0200

    fnf error coverage

--- openatlas/api/util.py
+++ openatlas/api/util.py
@@ -15,24 +15,26 @@ from openatlas.util.image_processing import ImageProcessing
 @app.route('/api/display/<path:filename>', strict_slashes=False)
 @cross_origin(origins=app.config['CORS_ALLOWANCE'], methods=['GET'])
 def display_file_api(filename: str) -> Any:
-    from pathlib import Path as Pathlib_path
-    entity = Entity.get_by_id(int(Pathlib_path(filename).stem), nodes=True)
-    license_ = None
-    for node in entity.nodes:
-        if node.root and node.root[-1] == Node.get_hierarchy('License').id:
-            license_ = node.name
-    if not license_:
-        raise AccessDeniedError
-
-    parser = image_parser.parse_args()
-    path = f"{app.config['UPLOAD_DIR']}/{filename}"
-    if parser['image_size']:
-        name = filename.rsplit('.', 1)[0].lower()
-        size = app.config['IMAGE_SIZE'][parser['image_size']]
-        if not ImageProcessing.check_if_processed_image_exist(name, size):
-            raise APIFileNotFoundError
-        path = f"{app.config['RESIZED_IMAGES']}/{size}/{name}.jpeg"
-    return send_file(path, as_attachment=True if parser['download'] else False)
+    try:
+        from pathlib import Path as Pathlib_path
+        entity = Entity.get_by_id(int(Pathlib_path(filename).stem), nodes=True)
+        license_ = None
+        for node in entity.nodes:
+            if node.root and node.root[-1] == Node.get_hierarchy('License').id:
+                license_ = node.name
+        if not license_:
+            raise AccessDeniedError
+        parser = image_parser.parse_args()
+        path = f"{app.config['UPLOAD_DIR']}/{filename}"
+        if parser['image_size']:
+            name = filename.rsplit('.', 1)[0].lower()
+            size = app.config['IMAGE_SIZE'][parser['image_size']]
+            if not ImageProcessing.check_if_processed_image_exist(name, size):
+                raise APIFileNotFoundError
+            path = f"{app.config['RESIZED_IMAGES']}/{size}/{name}.jpeg"
+        return send_file(path, as_attachment=True if parser['download'] else False)
+    except Exception:
+        raise APIFileNotFoundError
 
 
 @app.route('/api/0.1/', strict_slashes=False)
