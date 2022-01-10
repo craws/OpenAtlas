dba7861de747e6e3214ec811f72e3d83f4d99686
Alexander Watzinger <alexander.watzinger@craws.net>
2022-01-10 14:22:31 +0100

    Removed comment

--- openatlas/util/image_processing.py
+++ openatlas/util/image_processing.py
@@ -22,7 +22,6 @@ class ImageProcessing:
 
     @staticmethod
     def safe_resize_image(name: str, file_format: str, size: str) -> bool:
-        # With python3-wand 0.6. Path works and str conversation can be removed
         try:
             if ImageProcessing.check_if_folder_exist(
                     size,
