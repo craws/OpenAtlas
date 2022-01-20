a5ab2b66250c6f6b1ed5e8d28e9be18f283b9435
Alexander Watzinger <alexander.watzinger@craws.net>
2022-01-20 19:11:52 +0100

    Minor SQL update fix

--- install/upgrade/6.6.0.sql
+++ install/upgrade/6.6.0.sql
@@ -164,7 +164,7 @@ ALTER TABLE web.reference_system_openatlas_class ALTER COLUMN openatlas_class_na
 DROP TABLE web.form;
 
 -- Move source translation to standard type which seemed to have gone wrong in one of the last versions
-UPDATE web.hierarchy SET category = 'standard' WHERE name = 'Source translation';
+UPDATE web.hierarchy SET category = 'standard' WHERE name = 'Source Translation';
 
 -- #1500 Creation of artifacts - add event type to production
 INSERT INTO web.hierarchy_openatlas_class (hierarchy_id, openatlas_class_name) VALUES
