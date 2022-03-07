6ac2d6e48f2c5cba39bbaa8e418906f2ea9621b3
Alexander Watzinger <alexander.watzinger@craws.net>
2022-03-07 15:44:43 +0100

    Update NPM libs

--- openatlas/static/setup.py
+++ openatlas/static/setup.py
@@ -2,25 +2,29 @@ from setuptools import setup
 
 package_json = {
     "dependencies": {
-        "jquery": "^3.6.0",
-        "d3": "^5.16.0",
         "@fortawesome/fontawesome-free": "^5.15.4",
+        "bootstrap-autocomplete": "2.3.7",
+        "bootstrap": "^4.6.1",
+        "d3": "^5.16.0",
+        "datatables.net-bs4": "^1.11.5",
+        "datatables.net-buttons-dt": "^1.7.1",
+        "datatables.net-buttons": "^1.7.1",
+        "datatables.net-dt": "^1.11.5",
+        "datatables.net": "^1.11.5",
         "huebee": "^2.1.1",
         "jquery-ui-dist": "^1.13.1",
         "jquery-validation": "^1.19.3",
+        "jquery": "^3.6.0",
         "jstree": "^3.3.12",
-        "datatables.net": "^1.11.5",
-        "datatables.net-dt": "^1.11.5",
-        "datatables.net-buttons": "^1.7.1",
-        "datatables.net-buttons-dt": "^1.7.1",
-        "datatables.net-bs4": "^1.11.5",
-        "leaflet": "^1.7.1",
         "leaflet-imageoverlay-rotated": "^v0.2.1",
+        "leaflet.markercluster": "^1.5.3",
+        "leaflet-draw": "^1.0.4",
+        "leaflet-easybutton": "^2.4.0",
+        "leaflet-groupedlayercontrol": "^0.6.1",
+        "leaflet": "^1.7.1",
+        "popper.js": "^1.16.1",
         "save-svg-as-png": "^1.4.17",
         "tinymce": "^5.10.3",
-        "bootstrap": "^4.6.1",
-        "popper.js": "^1.16.1",
-        "bootstrap-autocomplete": "2.3.7"
     }
 }
 
