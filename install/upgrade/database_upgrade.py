de27b6d414885be4bd5fc3fe878720c47a439941
Alexander Watzinger <alexander.watzinger@craws.net>
2022-04-27 15:35:42 +0200

    Database upgrade script: check and exit if nothing to do.

--- install/upgrade/database_upgrade.py
+++ install/upgrade/database_upgrade.py
@@ -1,7 +1,9 @@
 # Script for upgrading the database to the current (software) version
 # Usage from project root:
 # python3 install/upgrade/database_upgrade.py
+
 import sys
+import time
 from pathlib import Path
 
 from psycopg2 import extras
@@ -15,13 +17,14 @@ from config.default import (
     DATABASE_HOST, DATABASE_PORT)
 from instance import production
 
-
 config = {
     'DATABASE_NAME': DATABASE_NAME,
     'DATABASE_USER': DATABASE_USER,
     'DATABASE_PASS': DATABASE_PASS,
     'DATABASE_PORT': DATABASE_PORT,
     'DATABASE_HOST': DATABASE_HOST}
+
+start = time.time()
 
 for item in config:
     try:
@@ -33,6 +36,18 @@ db = open_connection(config)
 cursor = db.cursor(cursor_factory=extras.DictCursor)
 settings = Settings.get_settings(cursor)
 
+
+def end_output():
+    print(f'Execution time: {int(time.time() - start)} seconds')
+    exit()
+
+
 print(f"{VERSION} OpenAtlas version")
 print(f"{DATABASE_VERSION} Database version required")
 print(f"{settings['database_version']} Installed database version")
+print('')
+if DATABASE_VERSION == settings['database_version']:
+    print(
+        'The current database version already matches the required one. '
+        'Have a nice day.')
+    end_output()
