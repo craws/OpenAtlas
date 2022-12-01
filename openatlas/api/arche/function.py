3fd84722ee070a0e51467f159702f9f3ee077220
BernhardKoschicek <b.koschicek@gmail.com>
2022-12-01 16:33:46 +0100

    changed dict names and refactored sort funtion

--- openatlas/api/arche/function.py
+++ openatlas/api/arche/function.py
@@ -2,7 +2,7 @@ from typing import Any
 
 import rdflib
 import requests
-from flask import Response
+from requests import Response
 
 from openatlas import app
 
@@ -13,55 +13,55 @@ def fetch_files() -> dict[int, Any]:
         req = requests.get(
             f"{app.config['ARCHE_BASE_URL']}/api/{id_}/metadata",
             headers={'Accept': 'application/n-triples'})
-        collections[id_] = filter_all_files(req)
-    print(collections)
-    # print(collections[1390141]['metadata']['https://arche-curation.acdh-dev.oeaw.ac.at/api/1390185']['isMetadataFor'])
-    # print(len(collections[1390141]['not_unique_values']))
-    # print(len(collections[1390141]['images']))
-
+        collections[id_] = sort_data(n_triples_to_json(req))
     return collections
 
 
-def filter_all_files(req: Response) -> dict[str, Any]:
+def sort_data(data: dict[str, Any]) -> dict[str, Any]:
     files = {
         'metadata': {},
         'not_unique_values': {},
-        'images': {}}
-    for uri, node in n_triples_to_json(req).items():
+        'jpeg': {},
+        'tiff': {}}
+    for uri, node in data.items():
+        uri_split = uri.rsplit('/', 1)[1]
         for prop, value in node.items():
-            if 'metadata.json' in str(value[0]):
-                files['metadata'][uri.rsplit('/', 1)[1]] = {
-                    'metadataARCHE': node,
-                    'metadataFile': requests.get(uri).json(),
-                    'isMetadataFor':
-                        get_jpeg_from_metadata(node['isMetadataFor'])}
-            if '_not_unique_values.json' in str(value[0]):
-                files['not_unique_values'][uri.rsplit('/', 1)[1]] = {
-                    'metadataARCHE': node,
-                    'metadataFile': requests.get(uri).json(),
-                    'isMetadataFor':
-                        get_jpeg_from_metadata(node['isMetadataFor'])}
-            if str(value[0]) == 'image/jpeg':
-                files['images'][uri.rsplit('/', 1)[1]] = {
-                    'metadataARCHE': node,
-                    'originalFileLink': uri,
-                    'thumbnailLink':
-                        f"{app.config['ARCHE_THUMBNAIL']}{uri.replace('https://', '')}?width=400"}
-            if str(value[0]) == 'image/tiff':
-                files['images'][uri.rsplit('/', 1)[1]] = {
-                    'metadataARCHE': node,
-                    'originalFileLink': uri,
-                    'thumbnailLink':
-                        f"{app.config['ARCHE_THUMBNAIL']}{uri.replace('https://', '')}?width=400"}
+            if 'metadata' in str(value[0]):
+                files['metadata'][uri_split] = json_dict(uri, node)
+            if 'not_unique_values' in str(value[0]):
+                files['not_unique_values'][uri_split] = json_dict(uri, node)
+            if str(value[0]) in ['image/jpeg', 'image/tiff']:
+                value = str(value[0].rsplit('/', 1)[1])
+                files[value][uri_split] = image_dict(uri, node)
     return files
 
 
-def get_jpeg_from_metadata(data: list[dict[str, Any]]) -> str:
+def json_dict(uri: str, node: dict[str, Any]) -> dict[str, Any]:
+    return {
+        'metadataARCHE': node,
+        'metadataFile': requests.get(uri).json(),
+        'isMetadataFor': get_linked_image_id(node['isMetadataFor'])}
+
+
+def image_dict(uri: str, node: dict[str, Any]) -> dict[str, Any]:
+    return {
+        'metadataARCHE': node,
+        'originalFileLink': uri,
+        'thumbnailLink':
+            f"{app.config['ARCHE_THUMBNAIL']}"
+            f"{uri.replace('https://', '')}?width=400"}
+
+
+def get_linked_image_id(data: list[dict[str, Any]]) -> str:
     for image in data:
         if str(image['mime'][0]) == 'image/jpeg':
             return image['__uri__'].rsplit('/', 1)[1]
 
 
+########################
+# Script from
+# https://acdh-oeaw.github.io/arche-docs/aux/rdf_compacting_and_framing.html
+########################
 def n_triples_to_json(req: Response) -> dict[str, Any]:
     context = get_arche_context()
     data = rdflib.Graph()
@@ -74,7 +74,6 @@ def n_triples_to_json(req: Response) -> dict[str, Any]:
         prop = str(prop)
         # skip RDF properties for which we don't know the mapping
         if prop not in context:
-            # print(prop)
             continue
 
         # map prop name according to the context
@@ -85,14 +84,16 @@ def n_triples_to_json(req: Response) -> dict[str, Any]:
         if not isinstance(obj, rdflib.term.Literal):
             if str(obj) not in nodes:
                 nodes[str(obj)] = {'__uri__': str(obj)}
-            obj = nodes[str(obj)]
+            obj = nodes[str(obj.toPython())]
 
         # manage the data
         if sbj not in nodes:
             nodes[sbj] = {'__uri__': sbj}
         if prop not in nodes[sbj]:
             nodes[sbj][prop] = []
-        nodes[sbj][prop].append(obj)
+        nodes[sbj][prop].append(
+            obj if not isinstance(obj, rdflib.term.Literal)
+            else obj.toPython())
     return nodes
 
 
