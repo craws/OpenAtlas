d4bd0bc3d8d329f5516f9d240d63873c27dec145
Alexander Watzinger <alexander.watzinger@craws.net>
2023-01-22 23:27:09 +0100

    Tests: file tests

--- tests/test_file.py
+++ tests/test_file.py
@@ -15,27 +15,16 @@ class FileTest(TestBaseCase):
             with app.test_request_context():
                 app.preprocess_request()  # type: ignore
                 place = insert_entity('place', 'File keeper')
-                reference = Entity.insert('edition', 'Ancient Books')
+                reference = insert_entity('edition', 'Ancient Books')
                 type_id = Type.get_hierarchy('Sex').subs[0]
 
-            rv = self.app.get(
-                url_for('insert', class_='file', origin_id=place.id))
-            assert b'+ File' in rv.data
-
-            logo = \
-                pathlib.Path(app.root_path) \
+            logo = pathlib.Path(app.root_path) \
                 / 'static' / 'images' / 'layout' / 'logo.png'
-            with open(logo, 'rb') as img:
-                rv = self.app.post(
-                    url_for('insert', class_='file', origin_id=place.id),
-                    data={'name': 'OpenAtlas logo', 'file': img},
-                    follow_redirects=True)
-            assert b'An entry has been created' in rv.data
 
-            with open(logo, 'rb') as img1, open(logo, 'rb') as img2:
+            with open(logo, 'rb') as img_1, open(logo, 'rb') as img_2:
                 rv = self.app.post(
                     url_for('insert', class_='file', origin_id=place.id),
-                    data={'name': 'OpenAtlas logo', 'file': [img1, img2]},
+                    data={'name': 'OpenAtlas logo', 'file': [img_1, img_2]},
                     follow_redirects=True)
             assert b'An entry has been created' in rv.data
 
@@ -50,17 +39,19 @@ class FileTest(TestBaseCase):
                 app.preprocess_request()  # type: ignore
                 files = Entity.get_by_class('file')
                 file_id = files[0].id
-                file_id2 = files[1].id
+
+            filename = f'{file_id}.png'
+            with self.app.get(url_for('display_logo', filename=filename)):
+                pass
+
+            with self.app.get(url_for('download_file', filename=filename)):
+                pass
 
             rv = self.app.get(
                 url_for('admin_logo'),
                 data={'file': file_id},
                 follow_redirects=True)
             assert b'OpenAtlas logo' in rv.data
-
-            filename = f'{file_id}.png'
-            with self.app.get(url_for('display_logo', filename=filename)):
-                pass
 
             rv = self.app.get(
                 url_for('admin_logo', id_=file_id),
@@ -81,36 +72,10 @@ class FileTest(TestBaseCase):
                     follow_redirects=True)
             assert b'File type not allowed' in rv.data
 
-            rv = self.app.post(
-                url_for('insert', class_='file', origin_id=place.id),
-                follow_redirects=True,
-                data={'name': 'This is not a file'})
-            assert b'This field is required' in rv.data
-
-            rv = self.app.get(url_for('view', id_=file_id))
-            assert b'OpenAtlas logo' in rv.data
-
-            rv = self.app.get(url_for('view', id_=file_id2))
-            assert b'OpenAtlas logo' in rv.data
-
-            with self.app.get(url_for('download_file', filename=filename)):
-                pass
-
-            with self.app.get(url_for('display_file', filename=filename)):
-                pass
-
-            rv = self.app.get(url_for('index', view='file'))
-            assert b'OpenAtlas logo' in rv.data
-
-            self.app.get(
-                url_for('set_profile_image', id_=file_id, origin_id=place.id),
-                follow_redirects=True)
-            self.app.get(
-                url_for('file_remove_profile_image', entity_id=place.id))
-
             rv = self.app.get(
-                url_for('reference_add', id_=reference.id, view='file'))
-            assert b'OpenAtlas logo' in rv.data
+                url_for('file_remove_profile_image', entity_id=place.id),
+                follow_redirects=True)
+            assert b'Unset' not in rv.data
 
             rv = self.app.post(
                 url_for('reference_add', id_=reference.id, view='file'),
@@ -118,15 +83,11 @@ class FileTest(TestBaseCase):
                 follow_redirects=True)
             assert b'777' in rv.data
 
-            rv = self.app.get(url_for('update', id_=file_id))
-            assert b'OpenAtlas logo' in rv.data
-
             rv = self.app.post(
                 url_for('update', id_=file_id),
                 data={'name': 'Updated file'},
                 follow_redirects=True)
             assert b'Changes have been saved' in rv.data
-            assert b'Updated file' in rv.data
 
             rv = self.app.get(url_for('file_add', id_=file_id, view='actor'))
             assert b'Link actor' in rv.data
@@ -146,16 +107,6 @@ class FileTest(TestBaseCase):
                 follow_redirects=True)
             assert b'Updated file' in rv.data
 
-            self.app.post(
-                url_for('insert', class_='source', origin_id=file_id),
-                data={'name': 'Created source coming from file'})
-            with app.test_request_context():
-                app.preprocess_request()  # type: ignore
-                source_id = Entity.get_by_class('source')[0].id
-            rv = self.app.get(url_for('view', id_=source_id))
-            assert b'Created source coming from file' in rv.data
-
-            # Delete
             for file in files:
                 rv = self.app.get(
                     url_for('index', view='file', delete_id=file.id))
