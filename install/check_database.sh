56979776f724605118acd08e8ab4c4320e0ba49f
bkoschicek <b.koschicek@gmail.com>
2023-01-23 14:47:41 +0100

    edit check_database.sh to not check, but install

--- install/check_database.sh
+++ install/check_database.sh
@@ -2,26 +2,26 @@
 
 pushd /var/www/openatlas || exit
 
-if [ "$( psql $DB_URL -tAc "SELECT 1 FROM pg_database WHERE datname='$POSTGRES_DB'" )" = '1' ]
-then
-    echo "Database already exists"
-    if [ "$( psql $DB_URL -tAc "SELECT 1 FROM $POSTGRES_DB.web.settings" )" = '1' ]
-    then
-      echo "Database has already data"
-    else
-      echo "Database is empty. Please install backup manually"
-    fi
-    pushd install/upgrade/ || exit
-    python3 ./database_upgrade.py || exit
-    popd || exit
-else
-    echo "Database does not exist"
-    echo "Install necessary postgres extensions"
-    psql $DB_URL -c "CREATE EXTENSION postgis; CREATE EXTENSION unaccent;"
+#if [ "$( psql $DB_URL -tAc "SELECT 1 FROM pg_database WHERE datname='$POSTGRES_DB'" )" = '1' ]
+#then
+#    echo "Database already exists"
+#    if [ "$( psql $DB_URL -tAc "SELECT 1 FROM $POSTGRES_DB.web.settings" )" = '1' ]
+#    then
+#      echo "Database has already data"
+#    else
+#      echo "Database is empty. Please install backup manually"
+#    fi
+#    pushd install/upgrade/ || exit
+#    python3 ./database_upgrade.py || exit
+#    popd || exit
+#else
+echo "Database does not exist"
+echo "Install necessary postgres extensions"
+psql $DB_URL -c "CREATE EXTENSION postgis; CREATE EXTENSION unaccent;"
 
-    echo "Import database dumps"
-    pushd /var/www/openatlas/install || exit
-    cat 1_structure.sql 2_data_model.sql 3_data_web.sql  4_data_node.sql | psql $DB_URL -f -
-    popd || exit
-fi
+echo "Import database dumps"
+pushd /var/www/openatlas/install || exit
+cat 1_structure.sql 2_data_model.sql 3_data_web.sql  4_data_node.sql | psql $DB_URL -f -
 popd || exit
+#fi
+#popd || exit
