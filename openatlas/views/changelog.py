74aa570d8fea6e8551b15136549c62c6668bf2ad
Alexander Watzinger <alexander.watzinger@craws.net>
2023-02-04 15:50:41 +0100

    Update changelog

--- openatlas/views/changelog.py
+++ openatlas/views/changelog.py
@@ -15,7 +15,7 @@ def index_changelog() -> str:
 
 
 versions = {
-    '7.10.0': ['TBA', {
+    '7.10.0': ['2023-02-04', {
         'feature': {
             '1934': 'New creation event class',
             '1090': 'Radiocarbon Dating',
