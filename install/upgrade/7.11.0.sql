a44c838a49753ded670fe55a98731b7751002463
Alexander Watzinger <alexander.watzinger@craws.net>
2023-02-19 14:10:55 +0100

    Update SQL upgrade comments

--- install/upgrade/7.11.0.sql
+++ install/upgrade/7.11.0.sql
@@ -6,10 +6,10 @@ BEGIN;
 -- Raise database version
 UPDATE web.settings SET value = '7.11.0' WHERE name = 'database_version';
 
--- Remove setting for show API buttons (#)
+-- Remove setting for show API buttons (#1963)
 DELETE FROM web.user_settings WHERE name = 'entity_show_api';
 
--- Remove system logs before 2022, comment this in case you want to keep them
+-- Remove system logs before 2022, delete this if you want to keep them (#1964)
 DELETE FROM web.system_log WHERE created < '2022-01-01';
 
 END;
