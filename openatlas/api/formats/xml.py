3e5486ee36ea5955e6c94471842a62053c15449b
BernhardKoschicek <b.koschicek@gmail.com>
2023-03-03 10:33:56 +0100

    test what breaks GitHub tests of loud (xml)

--- openatlas/api/formats/xml.py
+++ openatlas/api/formats/xml.py
@@ -1,7 +1,11 @@
+import logging
 from typing import Any
 
-from dicttoxml import dicttoxml
+import dicttoxml
 from flask import Response
+
+dicttoxml.set_debug(debug=False)
+dicttoxml.LOG.setLevel(logging.ERROR)
 
 
 def subunit_xml(result: dict[str, Any]) -> bytes:
@@ -10,7 +14,7 @@ def subunit_xml(result: dict[str, Any]) -> bytes:
     for key, value in result.items():
         item_list.append({item['openatlasClassName']: item} for item in value)
         new_dict[key] = item_list
-    xml = dicttoxml(new_dict, root=False, attr_type=False)
+    xml = dicttoxml.dicttoxml(new_dict, root=False, attr_type=False)
     xml = xml.replace(b'<item >', b'')
     xml = xml.replace(b'<item>', b'')
     xml = xml.replace(b'</item>', b'')
@@ -19,7 +23,7 @@ def subunit_xml(result: dict[str, Any]) -> bytes:
 
 def export_database_xml(tables: dict[str, Any], filename: str) -> Response:
     return Response(
-        dicttoxml(tables, root=False, attr_type=False),
+        dicttoxml.dicttoxml(tables, root=False, attr_type=False),
         mimetype='application/xml',
         headers={
             'Content-Disposition': f'attachment;filename={filename}.xml'})
