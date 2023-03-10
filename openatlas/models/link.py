6854a86d1ab84ce3f835f2f5655ca8010975dd14
Alexander Watzinger <alexander.watzinger@craws.net>
2023-03-10 15:07:37 +0100

    Fix for duplicate check

--- openatlas/models/link.py
+++ openatlas/models/link.py
@@ -250,17 +250,17 @@ class Link:
         from openatlas.models.entity import Entity
         data = []
         for type_ in g.types.values():
-            if not type_.multiple \
-                    and type_.category not in ['value', 'tools'] \
-                    and (type_ids := type_.get_sub_ids_recursive()):
-                for id_ in Db.check_single_type_duplicates(type_ids):
-                    offending_types = []
-                    entity = Entity.get_by_id(id_, types=True)
-                    for entity_type in entity.types:
-                        if g.types[entity_type.root[0]].id == type_.id:
-                            offending_types.append(entity_type)
-                    data.append({
-                        'entity': entity,
-                        'type': type_,
-                        'offending_types': offending_types})
+            if not type_.multiple and type_.category not in ['value', 'tools']:
+                if type_ids := type_.get_sub_ids_recursive():
+                    for id_ in Db.check_single_type_duplicates(type_ids):
+                        offending_types = []
+                        entity = Entity.get_by_id(id_, types=True)
+                        for entity_type in entity.types:
+                            if g.types[entity_type.root[0]].id == type_.id:
+                                offending_types.append(entity_type)
+                        if offending_types:
+                            data.append({
+                                'entity': entity,
+                                'type': type_,
+                                'offending_types': offending_types})
         return data
