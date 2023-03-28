13303f2534ef180473717d5650f8f4703e5c55f1
Alexander Watzinger <alexander.watzinger@craws.net>
2023-03-28 16:17:39 +0200

    Update changelog

--- openatlas/views/changelog.py
+++ openatlas/views/changelog.py
@@ -17,6 +17,7 @@ def index_changelog() -> str:
 versions = {
     '7.12.0': ['TBA', {
         'feature': {
+            '1753': 'Make a copy of an entry',
             '1946': 'Adding sub artifacts',
             '1993': 'Functionality to change super of subunits',
             '1975': 'Make description field wider',
@@ -25,8 +26,7 @@ versions = {
             '1987': 'API: Add centroid points of geometries',
             '1988': 'API: Depiction URL without extension',
             '1973': 'Manual: document linking artifact with places',
-            '1990': 'Refactor and minor improvements'}
-    }],
+            '1990': 'Refactor and minor improvements'}}],
     '7.11.1': ['2023-02-20', {
         'fix': {
             '1971': 'Validation for "Inventory number" is broken'}}],
