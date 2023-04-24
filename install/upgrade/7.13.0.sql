66697dce7dd681413861d771572ec21332661f42
Alexander Watzinger <alexander.watzinger@craws.net>
2023-04-24 19:09:54 +0200

    Fixed version numbers for upgrade

--- install/upgrade/7.13.0.sql
+++ install/upgrade/7.13.0.sql
@@ -1,9 +1,9 @@
 -- Upgrade 7.12.0 to 7.13.0
 -- Be sure to backup the database and read the upgrade notes before executing.
 
--- So far the database version wasn't raised for 7.12.0 so the database upgrade
+-- So far the database version wasn't raised for 7.13.0 so the database upgrade
 -- script won't execute this file. Reason for this is that there is more to
--- to come but feel free to execute it manually. So far SQL Code below won't
+-- to come but feel free to execute it manually. So far SQL code below won't
 -- be problematic if run again with the database upgrade script later.
 
 BEGIN;
