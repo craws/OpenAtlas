08eca1369f4c7c149f2ab2892c9970a00c8e1619
bkoschicek <b.koschicek@gmail.com>
2023-05-16 12:06:45 +0200

    added vocabs default dict

--- config/default.py
+++ config/default.py
@@ -91,11 +91,19 @@ ARCHE = {
     'base_url': None,
     'thumbnail_url': 'https://arche-thumbnails.acdh.oeaw.ac.at/'}
 
-# Used to connect to ACDH-CH VOCABS systems
+# Todo: to be deleted if not necessary anymore
 VOCABS = {
     'id': None,
     'api_uri': 'https://vocabs.acdh.oeaw.ac.at/rest/v1/',
     'uri': 'https://vocabs.acdh.oeaw.ac.at/'}
+
+# Used to connect to ACDH-CH VOCABS systems
+VOCABS_URI = {
+    'default': {
+        'uri': 'https://base.uri/',
+        'api': 'https://api.base.uri/v1/',
+        'user': '',
+        'password': ''}}
 
 # Table options
 TABLE_ROWS = {10: '10', 25: '25', 50: '50', 100: '100'}
