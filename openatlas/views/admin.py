39f68fb07db5376ec5ea69e4584f63f9809113c6
bkoschicek <b.koschicek@gmail.com>
2023-11-27 13:50:51 +0100

    print disk space info

--- openatlas/views/admin.py
+++ openatlas/views/admin.py
@@ -779,14 +779,13 @@ def get_disk_space_info() -> Optional[dict[str, Any]]:
     percent_free = 100 - math.ceil(stats.free / (stats.total / 100))
     percent_files = math.ceil(files_size / (stats.total / 100))
     other_files = stats.total - stats.free - files_size
-    print({
-        'total': convert_size(stats.total),
-        'project': convert_size(files_size),
-        'other_files': convert_size(other_files),
-        'free': convert_size(stats.free),
-        'percent_used': percent_free,
-        'percent_project': percent_files,
-        'percent_other': 100 - (percent_files + percent_free)})
+    print(files_size)
+    print(stats)
+    print(stats.free)
+    print(stats.total)
+    print(percent_free)
+    print(percent_files)
+    print(other_files)
     return {
         'total': convert_size(stats.total),
         'project': convert_size(files_size),
