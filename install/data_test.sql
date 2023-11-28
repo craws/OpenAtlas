8065ce3262176b04b06851a8421d3f848b8bbcce
Alexander Watzinger <alexander.watzinger@craws.net>
2023-11-28 14:24:19 +0100

    Tests

--- install/data_test.sql
+++ install/data_test.sql
@@ -33,3 +33,4 @@ UPDATE web.settings SET value = '/var/www/iipsrv' WHERE name = 'iiif_path';
 UPDATE web.settings SET value = 'http://localhost/iiif/' WHERE name = 'iiif_url';
 UPDATE web.settings SET value = '2' WHERE name = 'iiif_version';
 UPDATE web.settings SET value = 'deflate' WHERE name = 'iiif_conversion';
+UPDATE web.settings SET value = 'https://frontend-demo.openatlas.eu/entity/' WHERE name = 'frontend_resolver_url';
