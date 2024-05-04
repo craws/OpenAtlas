51aef2aa852c7cff58ffabeacb44a246d347ecb7
Alexander Watzinger <alexander.watzinger@craws.net>
2024-05-04 18:45:27 +0200

    Adaptions for upgrade SQL

--- install/upgrade/8.4.0.sql
+++ install/upgrade/8.4.0.sql
@@ -26,20 +26,28 @@ ALTER TABLE web.map_overlay ADD CONSTRAINT map_overlay_image_id_key UNIQUE (imag
 -- #2261 Option to prevent selection of a type
 DROP TABLE IF EXISTS web.type_none_selectable;
 DROP SEQUENCE IF EXISTS web.type_none_selectable_id_seq;
-CREATE SEQUENCE web.type_none_selectable_id_seq INCREMENT 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1;
+
+CREATE SEQUENCE web.type_none_selectable_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    MAXVALUE 2147483647
+    CACHE 1;
 ALTER TABLE web.type_none_selectable_id_seq OWNER TO openatlas;
 
 CREATE TABLE web.type_none_selectable (
-    "id" integer DEFAULT nextval('web.type_none_selectable_id_seq') NOT NULL,
-    "entity_id" integer NOT NULL,
-    "created" timestamp DEFAULT now() NOT NULL,
-    CONSTRAINT "type_none_selectable_pkey" PRIMARY KEY ("id")
+    id integer DEFAULT nextval('web.type_none_selectable_id_seq'::regclass) NOT NULL,
+    entity_id integer NOT NULL,
+    created timestamp without time zone DEFAULT now() NOT NULL
 );
-ALTER TABLE IF EXISTS web.type_none_selectable OWNER to openatlas;
+
+ALTER TABLE IF EXISTS web.type_none_selectable OWNER TO openatlas;
 COMMENT ON TABLE web.type_none_selectable IS 'IDs of types that are not meant to be selected, e.g. a category';
 
 ALTER TABLE ONLY web.type_none_selectable ADD CONSTRAINT entity_id_key UNIQUE (entity_id);
-ALTER TABLE ONLY web.type_none_selectable ADD CONSTRAINT "type_none_selectable_entity_id_fkey"
-    FOREIGN KEY (entity_id) REFERENCES model.entity(id) ON UPDATE CASCADE ON DELETE CASCADE NOT DEFERRABLE;
+ALTER TABLE ONLY web.type_none_selectable ADD CONSTRAINT type_none_selectable_pkey PRIMARY KEY (id);
+
+ALTER TABLE ONLY web.type_none_selectable ADD CONSTRAINT type_none_selectable_entity_id_fkey
+    FOREIGN KEY (entity_id) REFERENCES model.entity(id) ON UPDATE CASCADE ON DELETE CASCADE;
 
 END;
