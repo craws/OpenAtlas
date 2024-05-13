164a7fda5c33b6363af3bbb33c9ac09f492cf16b
Alexander Watzinger <alexander.watzinger@craws.net>
2024-05-13 14:29:49 +0200

    Update upgrade SQL for additional file info

--- install/upgrade/license.sql
+++ install/upgrade/license.sql
@@ -1,30 +1,36 @@
 BEGIN;
 
-DROP TABLE IF EXISTS web.license;
-DROP SEQUENCE IF EXISTS license_id_seq;
-CREATE SEQUENCE license_id_seq INCREMENT 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1;
+-- #2129: Attributions for files
+DROP TABLE IF EXISTS model.file_info;
+DROP SEQUENCE IF EXISTS file_info_id_seq;
 
-CREATE TABLE web.license (
-    "id" integer DEFAULT nextval('license_id_seq') NOT NULL,
-    "name" text NOT NULL,
-    "description" text,
-    "super_id" integer,
-    "url" text,
-    "created" timestamp NOT NULL,
-    "modified" timestamp,
-    CONSTRAINT "license_pkey" PRIMARY KEY ("id")
-) WITH (oids = false);
+CREATE SEQUENCE model.file_info_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    MAXVALUE 2147483647
+    CACHE 1;
+ALTER TABLE model.file_info_id_seq OWNER TO openatlas;
 
-INSERT INTO web.license (name, description) VALUES
-    ('public', 'Licenses that allow public sharing, e.g. displaying on a website or adding to a public archive.'),
-    ('restricted', 'Licenses that restrict the use for public sharing.'),
-    ('needs clarification', 'Licenses that have to be clarified.');
+CREATE TABLE model.file_info (
+    id integer DEFAULT nextval('file_info_id_seq'::regclass) NOT NULL,
+    entity_id integer,
+    public boolean DEFAULT false,
+    creator text,
+    license_holder text,
+    created timestamp without time zone DEFAULT now() NOT NULL,
+    modified timestamp,
+    CONSTRAINT file_info_pkey PRIMARY KEY ("id")
+);
+COMMENT ON TABLE model.file_info IS 'Indicates if public sharing of corresponding file is allowed.';
+ALTER TABLE model.file_info OWNER TO openatlas;
 
-INSERT INTO web.license (name, description, url, super_id) VALUES
-    ('Public domain', 'When a work is in the public domain, it is free for use by anyone for any purpose without restriction under copyright law.', 'https://wiki.creativecommons.org/wiki/Public_domain', (SELECT id FROM web.license WHERE name = '')),
-    ('CC BY 4.0', 'See https://creativecommons.org/licenses/by/4.0/', 'https://creativecommons.org/licenses/by/4.0/', (SELECT id FROM web.license WHERE name = 'public')),
-    ('CC BY-SA 4.0', 'See https://creativecommons.org/licenses/by-sa/4.0/', 'https://creativecommons.org/licenses/by-sa/4.0/', (SELECT id FROM web.license WHERE name = 'public')),
-    ('Unspecified restricted', 'This is an example entry in case the kind of restriction is not known.', NULL, (SELECT id FROM web.license WHERE name = 'restricted')),
-    ('Needs clarification', 'This is an example entry', NULL, (SELECT id FROM web.license WHERE name = 'needs clarification'));
+ALTER TABLE model.file_info ADD CONSTRAINT entity_id_key UNIQUE (entity_id);
+ALTER TABLE model.file_info ADD CONSTRAINT type_none_selectable_entity_id_fkey
+    FOREIGN KEY (entity_id) REFERENCES model.entity(id) ON UPDATE CASCADE ON DELETE CASCADE;
+
+CREATE TRIGGER update_modified BEFORE
+UPDATE ON model.file_info
+FOR EACH ROW EXECUTE FUNCTION model.update_modified();
 
 END;
