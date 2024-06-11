f1955e8f77da1572f1c39bc9e47ec56852a97e74
Alexander Watzinger <alexander.watzinger@craws.net>
2024-06-11 15:47:00 +0200

    Shortened test_api file

--- tests/base.py
+++ tests/base.py
@@ -1,6 +1,6 @@
 import unittest
 from pathlib import Path
-from typing import Optional
+from typing import Any, Optional
 
 import psycopg2
 from flask import url_for
@@ -62,6 +62,44 @@ class ApiTestCase(TestBaseCase):
                 Path(app.root_path).parent / 'install' / 'data_test_api.sql',
                 encoding='utf8') as sql_file:
             self.cursor.execute(sql_file.read())
+
+    @staticmethod
+    def get_bool(
+            data: dict[str, Any],
+            key: str,
+            value: Optional[str | list[Any]] = None) -> bool:
+        return bool(data[key] == value) if value else bool(data[key])
+
+    @staticmethod
+    def get_bool_inverse(data: dict[str, Any], key: str) -> bool:
+        return bool(not data[key])
+
+    @staticmethod
+    def get_no_key(data: dict[str, Any], key: str) -> bool:
+        return bool(key not in data.keys())
+
+    @staticmethod
+    def get_geom_properties(geom: dict[str, Any], key: str) -> bool:
+        return bool(geom['features'][0]['properties'][key])
+
+    @staticmethod
+    def get_classes(data: list[dict[str, Any]]) -> bool:
+        return bool(
+            data[0]['systemClass']
+            and data[0]['crmClass']
+            and data[0]['view']
+            and data[0]['icon']
+            and data[0]['en'])
+
+    @staticmethod
+    def get_class_mapping(data: dict[str, Any], locale: str) -> bool:
+        return bool(
+            data['locale'] == locale
+            and data['results'][0]['systemClass']
+            and data['results'][0]['crmClass']
+            and data['results'][0]['view']
+            and data['results'][0]['icon']
+            and data['results'][0]['label'])
 
 
 class ExportImportTestCase(TestBaseCase):
