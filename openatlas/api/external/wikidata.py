2d624d6fa908f66dd59e66786c6cdf965c39e05b
Alexander Watzinger <alexander.watzinger@craws.net>
2024-07-09 16:47:03 +0200

    Pylint checks

--- openatlas/api/external/wikidata.py
+++ openatlas/api/external/wikidata.py
@@ -22,36 +22,34 @@ def fetch_wikidata(id_: str) -> dict[str, Any]:
         'languages': 'en'}
     info = {}
     try:
-        data = requests.get(url, params=params).json()
-    except:  # pragma: no cover
+        data = requests.get(url, params=params, timeout=10).json()
+    except Exception:  # pragma: no cover
         return {}
     try:
         info['title'] = data['entities'][id_]['labels']['en']['value']
-    except:  # pragma: no cover
+    except Exception:  # pragma: no cover
         pass
     try:
         info['aliases'] = [
             ' ' + v['value'] for v in data['entities'][id_]['aliases']['en']]
-    except:  # pragma: no cover
+    except Exception:  # pragma: no cover
         pass
     try:
         info['description'] = \
             data['entities'][id_]['descriptions']['en']['value']
-    except:  # pragma: no cover
+    except Exception:  # pragma: no cover
         pass
-
     try:
         info['founded by'] = [
             add_resolver_url(v['mainsnak']['datavalue']['value']['numeric-id'])
             for v in data['entities'][id_]['claims']['P112']]
-    except:
+    except Exception:
         pass
-
     try:
         info['nick names'] = [
             v['mainsnak']['datavalue']['value']['text']
             for v in data['entities'][id_]['claims']['P1449']]
-    except:
+    except  Exception:
         pass
     try:
         info['official websites'] = [
@@ -60,22 +58,20 @@ def fetch_wikidata(id_: str) -> dict[str, Any]:
                 v['mainsnak']['datavalue']['value'],
                 external=True)
             for v in data['entities'][id_]['claims']['P856']]
-    except:  # pragma: no cover
+    except Exception:  # pragma: no cover
         pass
     try:
         info['categories'] = [
             add_resolver_url(v['mainsnak']['datavalue']['value']['numeric-id'])
             for v in data['entities'][id_]['claims']['P910']]
-    except:  # pragma: no cover
+    except Exception:  # pragma: no cover
         pass
-
     try:
         info['inception'] = \
             data['entities'][id_]['claims']['P571'][0]['mainsnak'][
                 'datavalue']['value']['time']
-    except:  # pragma: no cover
+    except Exception:  # pragma: no cover
         pass
-
     try:
         info['latitude'] = \
             data['entities'][id_]['claims']['P625'][0]['mainsnak'][
@@ -83,6 +79,6 @@ def fetch_wikidata(id_: str) -> dict[str, Any]:
         info['longitude'] = \
             data['entities'][id_]['claims']['P625'][0]['mainsnak'][
                 'datavalue']['value']['longitude']
-    except:  # pragma: no cover
+    except Exception:  # pragma: no cover
         pass
     return info
