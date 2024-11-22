f84261c4fd9f7b7294905810e800fa4e1a886ded
Alexander Watzinger <alexander.watzinger@craws.net>
2024-11-22 16:55:45 +0100

    Raised version, update changelog

--- openatlas/views/changelog.py
+++ openatlas/views/changelog.py
@@ -16,6 +16,11 @@ def index_changelog() -> str:
 
 # pylint: disable=too-many-lines
 versions = {
+    '8.9.0': ['TBA', {
+        'feature': {
+            '2389': 'Activity log for entities',
+            '2381': 'Import only year dates',
+            '2391': 'IIIF: add reference '}}],
     '8.8.0': ['2024-10-31', {
         'feature': {
             '2349': 'Stronger hints that notes are not part of the model',
