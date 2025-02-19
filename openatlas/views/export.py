775d1a649a4bd71aab4b06fe564133f3fd53ae56
Alexander Watzinger <alexander.watzinger@craws.net>
2025-02-19 14:14:54 +0100

    Fix for sql file table

--- openatlas/views/export.py
+++ openatlas/views/export.py
@@ -57,7 +57,7 @@ def export_sql() -> str:
                     _('delete'),
                     url_for('delete_export', filename=file.name),
                     js=f"return confirm('{confirm}')"))
-            table.rows.append(data)
+        table.rows.append(data)
     return render_template(
         'tabs.html',
         tabs={
