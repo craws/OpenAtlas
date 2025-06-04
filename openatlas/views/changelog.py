456a4c79ca7baac2399b1e041a613fd1530059bb
Alexander Watzinger <alexander.watzinger@craws.net>
2025-06-04 15:33:33 +0200

    Update changelog

--- openatlas/views/changelog.py
+++ openatlas/views/changelog.py
@@ -19,6 +19,7 @@ versions = {
         'feature': {
             '2507': 'Link button for reference at artifact tab',
             '2553': 'Removal of the SQL interface',
+            '2554': 'Refining input validation',
             '2526': 'API: Ego network visualization',
             '2477': 'API: Presentation view improvements',
             '2527': 'API: 0.4.8',
