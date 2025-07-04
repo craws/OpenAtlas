a7f2a92866eed7c4fe5bc9a495df6f5473ef225a
Bernhard Koschicek-Krombholz <b.koschicek@gmail.com>
2025-07-04 12:41:26 +0200

    tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -348,6 +348,10 @@ class Api(ApiTestCase):
         assert rv['relations']['feature']
         assert rv['relations']['person']
 
+        rv = c.get(url_for('api_04.entity_presentation_view', id_=place.id))
+        rv = rv.get_json()
+        assert rv['id'] == place.id
+
         rv = c.get(url_for('api_04.entity_presentation_view', id_=actor2.id))
         rv = rv.get_json()
         assert rv['id'] == actor2.id
