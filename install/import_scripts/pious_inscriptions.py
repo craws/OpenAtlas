5df9fabad6e413e719c2209b1e36139b4f95c3b6
Bernhard Koschicek-Krombholz <b.koschicek@gmail.com>
2025-09-29 12:57:04 +0200

    added artefact

--- install/import_scripts/pious_inscriptions.py
+++ install/import_scripts/pious_inscriptions.py
@@ -35,7 +35,7 @@ def clean_data() -> None:
         row['placement'] = row["placement"].rstrip(".")  # remove trailing .
 
 
-def get_admin_units():
+def get_admin_units() -> dict[str, Entity]:
     admin_units_: dict[str, Entity] = {}
     for entry in data:
         if entry['site'] in admin_units_:
@@ -53,7 +53,8 @@ def get_admin_units():
         admin_units_[entry['site']] = unit
     return admin_units_
 
-def get_provinces():
+
+def get_provinces() -> dict[str, Entity]:
     provinces_: dict[str, Entity] = {}
     for entry in data:
         if ", Province of" in entry['site']:
@@ -66,10 +67,13 @@ def get_provinces():
             provinces_[province_name] = province
     return provinces_
 
-def get_places():
+
+def get_places() -> dict[str, Entity]:
+    places_control_group: dict[str, Entity] = {}
     places_: dict[str, Entity] = {}
     for entry in data:
-        if entry['placement'] in places_:
+        if entry['placement'] in places_control_group:
+            places_[entry['id_string']] = places_control_group[entry['placement']]
             continue
         place = Entity.insert('place', entry['placement'])
         place.link('P2', case_study)
@@ -78,8 +82,42 @@ def get_places():
             f"Location of {entry['placement']}")
         place.link('P53', location)
         location.link('P89', admin_units[entry['site']])
-        places_[entry['placement']] = place
+        places_control_group[entry['placement']] = place
+        places_[entry['id_string']] = place
     return places_
+
+def get_current_location_types()-> dict[str, Entity]:
+    current_locations_: dict[str, Entity] = {}
+    for entry in data:
+        if not entry['current_location'] :
+            continue
+        if entry['current_location'] in current_locations_:
+            continue
+        current_location = Entity.insert('type', entry['current_location'])
+        current_location.link('P127', current_location_hierarchy)
+        current_locations_[entry['current_location']] = current_location
+    return current_locations_
+
+def get_artifacts() -> dict[str, Entity]:
+    artifacts_: dict[str, Entity] = {}
+    for entry in data:
+        if entry['id_string'] in artifacts_:
+            continue
+        # todo: Maybe move commentary to source itself as new text with
+        #   type "commentary"
+        description = f"""
+        {entry['site2']}\n{entry['description']}\n{entry['commentary']}"""
+        artifact = Entity.insert('artifact', entry['id_string'], description)
+        location = Entity.insert(
+            'object_location',
+            f"Location of {entry['id_string']}")
+        artifact.link('P53', location)
+        #artifact.link('P46', places[entry['id_string']])
+        if entry.get('current_location'):
+            artifact.link('P2', current_locations[entry['current_location']])
+    return artifacts_
+
+
 
 
 data = get_pious_data_from_db()
@@ -87,12 +125,15 @@ clean_data()
 
 # Just for debugging reasons
 df = pd.DataFrame(data)
-df.to_csv("inscriptions.csv", index=False, encoding="utf-8")
+df.to_csv("files/inscriptions.csv", index=False, encoding="utf-8")
 
 with app.test_request_context():
     app.preprocess_request()
     case_study = Entity.get_by_id(784)
     admin_hierarchy = Entity.get_by_id(81)
+    current_location_hierarchy = Entity.get_by_id(786)
     provinces = get_provinces()
     admin_units = get_admin_units()
     places = get_places()
+    current_locations = get_current_location_types()
+    artifacts = get_artifacts()
