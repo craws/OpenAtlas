7779c3dbd56eb42603f76ccb906d6f18ead9a4cf
Alexander Watzinger <alexander.watzinger@craws.net>
2026-01-05 00:06:11 +0100

    Update version and changelog

--- openatlas/views/changelog.py
+++ openatlas/views/changelog.py
@@ -15,6 +15,10 @@ def index_changelog() -> str:
 
 
 versions = {
+    '9.1.0': ['TBA', {
+        'feature': {
+            '2695': 'Refactor and minor improvements'
+        }}],
     '9.0.0': ['2026-01-01', {
         'feature': {
             '2464': 'OpenAtlas major version 9.0.0',
