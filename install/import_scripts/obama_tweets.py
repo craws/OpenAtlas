804510ecbe4612786d6287d3485c4d1676f3f9f0
Alexander Watzinger <alexander.watzinger@craws.net>
2026-02-20 16:31:41 +0100

    Continued Obama tweet import

--- install/import_scripts/obama_tweets.py
+++ install/import_scripts/obama_tweets.py
@@ -8,26 +8,23 @@ from typing import Final
 from openatlas import app
 from openatlas.models.entity import Entity, insert
 
-FILE_PATH: Final = Path("install/import_scripts/obama.csv")
+FILE_PATH: Final = Path("install/import_scripts/obama_personal.csv")
 
 
 @dataclass
 class Entry:
     tweet_id: str
     tweet_link: str
-    reply_to_status: bool
     timestamp: datetime
     text: str
     hashtags: list[str | None]
     usernames: list[str | None]
-    retweeted_status: bool
-    expanded_urls: str
 
 
 def import_csv_data(file_path: Path) -> list[Entry]:
     entries_csv: list[Entry] = []
     with open(file_path, mode="r", encoding="utf-8-sig") as f:
-        reader = csv.DictReader(f, delimiter=",")
+        reader = csv.DictReader(f, delimiter=";")
         for row in reader:
             entries_csv.append(process_row(row))
     return entries_csv
@@ -36,24 +33,16 @@ def import_csv_data(file_path: Path) -> list[Entry]:
 def process_row(row: dict[str, str]) -> Entry:
     tweet_id: str = row.get("tweet_id", "").strip()
     tweet_link: str = row.get("tweet_link", "").strip()
-    reply_to_status: bool = bool(
-        row.get("in_reply_to_status_id", False).strip())
     timestamp_str: str = row.get("timestamp", "").strip()
     text: str = row.get("text", "").strip()
-    retweeted_status: bool = bool(
-        row.get("retweeted_status_id", False).strip())
-    expanded_urls: str = row.get("expanded_urls", "").strip()
 
     return Entry(
         tweet_id=tweet_id,
         tweet_link=tweet_link,
-        reply_to_status=reply_to_status,
-        timestamp=datetime.strptime(timestamp_str, "%Y-%m-%d %H:%M:%S %z"),
+        timestamp=datetime.strptime(timestamp_str, "%Y/%m/%d_%H:%M"),
         text=text,
         hashtags=re.findall(r"#\w+", text),
-        usernames=re.findall(r"@\w+", text),
-        retweeted_status=retweeted_status,
-        expanded_urls=expanded_urls)
+        usernames=re.findall(r"@\w+", text))
 
 
 def get_hashtag_types(entries_: list[Entry]) -> dict[str, Entity]:
@@ -73,22 +62,27 @@ def get_hashtag_types(entries_: list[Entry]) -> dict[str, Entity]:
 def get_account_types(entries_: list[Entry]) -> dict[str, Entity]:
     account_types_: dict[str, Entity] = {}
     for entry_ in entries_:
-        for username in entry_.usernames:
-            if username in account_types_:
+        for username_ in entry_.usernames:
+            if username_ in account_types_:
                 continue
             type_ = insert({
-                'name': username,
+                'name': username_,
                 'openatlas_class_name': 'type'})
             type_.link('P127', account_hierarchy)
-            account_types_[username] = type_
+            account_types_[username_] = type_
     return account_types_
 
 
 with app.test_request_context():
     app.preprocess_request()
+    sources = Entity.get_by_class('source')
+    type_ = Entity.get_by_id(1314)
+    for source in sources:
+        source.link('P2', type_)
+
+
+    1/0
     entries = import_csv_data(FILE_PATH)
-    replay_type = Entity.get_by_id(151)
-    retweet_type = Entity.get_by_id(132)
     source_type = Entity.get_by_id(129)
     link_type = Entity.get_by_id(10)
     hashtag_hierarchy = Entity.get_by_id(247)
@@ -104,12 +98,6 @@ with app.test_request_context():
             'description': entry.text,
             'openatlas_class_name': 'source'})
         tweet.link('P2', source_type)
-
-        if entry.reply_to_status:
-            tweet.link('P2', replay_type)
-
-        if entry.retweeted_status:
-            tweet.link('P2', retweet_type)
 
         if entry.hashtags:
             for hashtag in entry.hashtags:
