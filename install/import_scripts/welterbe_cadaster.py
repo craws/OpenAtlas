be9cb040497ae41aebe47692fdc2787a9dd64573
Bernhard Koschicek-Krombholz <b.koschicek@gmail.com>
2026-03-03 16:54:28 +0100

    adapted script to process data in folder

--- install/import_scripts/welterbe_cadaster.py
+++ install/import_scripts/welterbe_cadaster.py
@@ -8,29 +8,28 @@ from openatlas.models.entity import Entity, insert
 
 # Download data from (Tab 'Distributionen/Distributions')
 # https://www.data.gv.at/datasets/5a56bef7-7b60-4822-9da7-1d118f312a4d
-# and store the needed CSV files in install/import_scripts/
+# and store the needed CSV files in install/import_scripts/cadaster_data/
 
-FILES: list[str] = [
-    # 'GST40015_V2.csv',
-    'GST40111_V2.csv']
+# Change the ID for the correct cadaster hierarchy
+CADASTER_HIERARCHY_ID = 221484
 
 
 def import_csv_data() -> dict[str, set[str]]:
     entries_csv = defaultdict(set)
-    for file_name in FILES:
-        path = Path('install/import_scripts/') / file_name
-        with open(path, mode="r", encoding="utf-8") as f:
-            reader = csv.DictReader(f, delimiter=";")
-            for row in reader:
-                entries_csv[row["KG-NR"]].add(row["GST-NR"])
+    data_dir = Path('install/import_scripts/cadaster_data')
+    for path in data_dir.iterdir():
+        if path.is_file() and path.name != '.gitignore':
+            with open(path, mode="r", encoding="utf-8") as f:
+                reader = csv.DictReader(f, delimiter=";")
+                for row in reader:
+                    entries_csv[row["KG-NR"]].add(row["GST-NR"])
     return entries_csv
 
 
 with app.test_request_context():
     app.preprocess_request()
 
-    # Change the ID for the correct cadaster hierarchy
-    cadaster_hierarchy = Entity.get_by_id(221484)
+    cadaster_hierarchy = Entity.get_by_id(CADASTER_HIERARCHY_ID)
     cadaster_reference_system = get_reference_system_by_name('Cadaster')
     entries = import_csv_data()
     cadaster_hierarchy_subs = {
