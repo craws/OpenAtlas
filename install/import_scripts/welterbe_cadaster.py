9c3534d0bb0974e5dd15202aa80cb1f8fa9e9a8a
Bernhard Koschicek-Krombholz <b.koschicek@gmail.com>
2026-03-03 16:44:32 +0100

    added import script for cadaster data

---
+++ install/import_scripts/welterbe_cadaster.py
@@ -0,0 +1,63 @@
+import csv
+from collections import defaultdict
+from pathlib import Path
+
+from openatlas import app
+from openatlas.api.import_scripts.util import get_reference_system_by_name
+from openatlas.models.entity import Entity, insert
+
+# Download data from (Tab 'Distributionen/Distributions')
+# https://www.data.gv.at/datasets/5a56bef7-7b60-4822-9da7-1d118f312a4d
+# and store the needed CSV files in install/import_scripts/
+
+FILES: list[str] = [
+    # 'GST40015_V2.csv',
+    'GST40111_V2.csv']
+
+
+def import_csv_data() -> dict[str, set[str]]:
+    entries_csv = defaultdict(set)
+    for file_name in FILES:
+        path = Path('install/import_scripts/') / file_name
+        with open(path, mode="r", encoding="utf-8") as f:
+            reader = csv.DictReader(f, delimiter=";")
+            for row in reader:
+                entries_csv[row["KG-NR"]].add(row["GST-NR"])
+    return entries_csv
+
+
+with app.test_request_context():
+    app.preprocess_request()
+
+    # Change the ID for the correct cadaster hierarchy
+    cadaster_hierarchy = Entity.get_by_id(221484)
+    cadaster_reference_system = get_reference_system_by_name('Cadaster')
+    entries = import_csv_data()
+    cadaster_hierarchy_subs = {
+        e.name: e for e in Entity.get_by_ids(cadaster_hierarchy.subs)}
+
+    for kg_name, gst_list in entries.items():
+        if kg_entity := cadaster_hierarchy_subs.get(kg_name):
+            kg_subs = {e.name for e in Entity.get_by_ids(kg_entity.subs)}
+            gst_list.difference_update(kg_subs)
+        else:
+            kg_entity = insert({
+                'name': kg_name,
+                'openatlas_class_name': 'administrative_unit'})
+            kg_entity.link('P89', cadaster_hierarchy)
+            kg_entity.link(
+                'P67',
+                cadaster_reference_system,
+                kg_name,
+                inverse=True)
+
+        for gst_name in gst_list:
+            gst_entity = insert({
+                'name': gst_name,
+                'openatlas_class_name': 'administrative_unit'})
+            gst_entity.link('P89', kg_entity)
+            gst_entity.link(
+                'P67',
+                cadaster_reference_system,
+                f'{kg_name}/{gst_name}',
+                inverse=True)
